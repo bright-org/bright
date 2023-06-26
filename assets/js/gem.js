032510b08416a293f172231bf2483d5277337121
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-06-26 14:23:09 +0900

    js リファクタリング

--- assets/js/gem.js
+++ assets/js/gem.js
@@ -1,5 +1,3 @@
-// TODO: chart.jsのimportの位置がここで良いか確認
-
 import { Chart } from 'chart.js/auto';
 export const gemHooks = {
   gem: {
@@ -13,22 +11,21 @@ export const gemHooks = {
 };
 
 const gem = (element) => {
-  var data = JSON.parse(element.dataset.data);
-  var labels = JSON.parse(element.dataset.labels);
-  var data2 = undefined;
-
-  var color = getColorPattern(data.length, ["#82FAE9", "#40DEC6", "#52CCB5"]);
-  var color2 = getColorPattern(data.length, ["#E4BDE955", "#C063CD55", "#9510B155"]);
-
+  const dataset = element.dataset;
+  const labels = JSON.parse(dataset.labels);
+  const data = JSON.parse(dataset.data);
+  let data2 = undefined;
+  const color = getColorPattern(data.length, ["#82FAE9", "#40DEC6", "#52CCB5"]);
+  const color2 = getColorPattern(data.length, ["#E4BDE955", "#C063CD55", "#9510B155"]);
   const datasets = [];
   datasets.push(createData(data));
 
-  if (element.dataset.data2.length != 0) {
-    data2 = JSON.parse(element.dataset.data2);
+  if (dataset.data2.length != 0) {
+    data2 = JSON.parse(dataset.data2);
     datasets.push(createData(data2));
   }
 
-  var chartJson = {
+  const chartJson = {
     type: 'radar',
     data: {
       labels: labels,
@@ -80,14 +77,14 @@ const gem = (element) => {
     plugins: [{ beforeDatasetsDraw: beforeDatasetsDraw }]
   };
 
-  var ctx = document.querySelector('#' + element.id + ' canvas');
-  var myChart = new Chart(ctx, chartJson);
+  const ctx = document.querySelector('#' + element.id + ' canvas');
+  const myChart = new Chart(ctx, chartJson);
 
   function beforeDatasetsDraw(chart) {
-    var context = chart.ctx;
-    var data = chart.data.datasets[0].data;
+    const context = chart.ctx;
+    const data = chart.data.datasets[0].data;
 
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       fillSurface(chart, data, i, color[i]);
     }
 
@@ -97,36 +94,36 @@ const gem = (element) => {
       }
     }
 
-    for (var i = 1; i < 5; i++) {
+    for (let i = 1; i < 5; i++) {
       drawGridline(chart, 20 * i, data.length);
     }
 
-    var img = new Image();
+    const img = new Image();
     img.src = "/images/icon_001.png";
 
     ///padding rightで拡張しないと、一番右にに表示するアイコンが削れる
     img.onload = function () {
-      for (var i = 0; i < data.length; i++) {
+      for (let i = 0; i < data.length; i++) {
         let label = chart.scales.r.getPointLabelPosition(i);
         context.drawImage(img, label.right + 2, label.top - 5, 20, 20);
       }
     }
 
-    for (var i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       drawUnderline(chart, i)
     }
   }
 
   function fillSurface(chart, data, index, color) {
-    var context = chart.ctx;
+    const context = chart.ctx;
 
-    startValue = data[(index % data.length)];
-    endValue = data[((index + 1) % data.length)];
-    endIndex = ((index + 1) % data.length);
+    const startValue = data[(index % data.length)];
+    const endValue = data[((index + 1) % data.length)];
+    const endIndex = ((index + 1) % data.length);
 
-    var v0 = chart.scales.r.getPointPositionForValue(0, 0);
-    var v1 = chart.scales.r.getPointPositionForValue(index, startValue);
-    var v2 = chart.scales.r.getPointPositionForValue(endIndex, endValue);
+    const v0 = chart.scales.r.getPointPositionForValue(0, 0);
+    const v1 = chart.scales.r.getPointPositionForValue(index, startValue);
+    const v2 = chart.scales.r.getPointPositionForValue(endIndex, endValue);
     context.beginPath();
     context.strokeStyle = color;
     context.moveTo(v0.x, v0.y);
@@ -138,15 +135,15 @@ const gem = (element) => {
   }
 
   function drawGridline(chart, value, length) {
-    var context = chart.ctx;
-    var v0 = chart.scales.r.getPointPositionForValue(0, value);
+    const context = chart.ctx;
+    const v0 = chart.scales.r.getPointPositionForValue(0, value);
     context.beginPath();
     context.moveTo(v0.x, v0.y);
     context.strokeStyle = "#FFFFFF44";
 
-    for (var i = 1; i < length; i++) {
+    for (let i = 1; i < length; i++) {
       chart.scales.r.getPointPositionForValue(i, value);
-      var v1 = chart.scales.r.getPointPositionForValue(i, value);
+      let v1 = chart.scales.r.getPointPositionForValue(i, value);
       context.lineTo(v1.x, v1.y);
     }
     context.lineTo(v0.x, v0.y);
@@ -154,8 +151,8 @@ const gem = (element) => {
   }
 
   function drawUnderline(chart, i) {
-    var context = chart.ctx;
-    var label = chart.scales.r.getPointLabelPosition(i);
+    const context = chart.ctx;
+    const label = chart.scales.r.getPointLabelPosition(i);
     context.beginPath();
     context.lineWidth = 1;
     context.strokeStyle = '#0000ff';
@@ -174,16 +171,16 @@ const gem = (element) => {
     length = myChart.data.labels.length;
 
     // リンクの判定例
-    for (var i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
       let label = myChart.scales.r.getPointLabelPosition(i);
-      var judge = (x >= label.left) && (x <= label.right) && (y >= label.top) && (y <= label.bottom)
+      let judge = (x >= label.left) && (x <= label.right) && (y >= label.top) && (y <= label.bottom)
       if (judge) { alert('リンククリック：' + myChart.data.labels[i]) }
     }
 
     // アイコン判定例
-    for (var i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
       let label = myChart.scales.r.getPointLabelPosition(i);
-      var judge = (x >= label.right + 2) && (x <= label.right + 20 + 2) && (y >= label.top - 5) && (y <= label.top + 20 - 5)
+      let judge = (x >= label.right + 2) && (x <= label.right + 20 + 2) && (y >= label.top - 5) && (y <= label.top + 20 - 5)
       if (judge) { alert('アイコンクリック：' + myChart.data.labels[i]) }
     }
 
@@ -195,7 +192,7 @@ const gem = (element) => {
 
     // lengthが4の時は先頭から2色の交互
     if (length == 4) {
-      for (var i = 0; i < length; i++) {
+      for (let i = 0; i < length; i++) {
         pattern.push(colors[i % 2]);
       }
       return pattern;
@@ -203,14 +200,14 @@ const gem = (element) => {
 
     // lengthが3で割り切れる時
     if (length % 3 == 0) {
-      for (var i = 0; i < length; i++) {
+      for (let i = 0; i < length; i++) {
         pattern.push(colors[i % colors.length]);
       }
       return pattern;
     }
 
     // lengthが3で割り切れるない時は3色の繰り返しで、最後は先頭から2番目の色を代入
-    for (var i = 0; i < length - 1; i++) {
+    for (let i = 0; i < length - 1; i++) {
       pattern.push(colors[i % colors.length]);
     }
     pattern.push(colors[1]);
