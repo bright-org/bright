09b321a8cc2af1868a4b8d272ce6d18dfc2bcf63
koyo-miyamura <nexus9071@gmail.com>
2023-06-27 15:46:11 +0900

    Add User and UserToken factories

---
+++ test/factories/user_factory.ex
@@ -0,0 +1,23 @@
+defmodule Bright.UserFactory do
+  @moduledoc """
+  Factory for Bright.Accounts.User
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+      def user_factory do
+        %Bright.Accounts.User{
+          email: sequence(:email, &"user#{&1}@example.com"),
+          hashed_password: Bcrypt.hash_pwd_salt(valid_user_password())
+        }
+      end
+
+      def user_with_password_factory do
+        build(:user, password: valid_user_password())
+      end
+
+      def unique_user_email, do: "user#{System.unique_integer()}@example.com"
+      def valid_user_password, do: "hello world!"
+    end
+  end
+end
