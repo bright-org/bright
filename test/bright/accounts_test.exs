af58e5ae24a92dc26b49fc7ed51f2f8494dc3ef8
koyo-miyamura <nexus9071@gmail.com>
2023-06-27 16:57:04 +0900

    Fix factory name

--- test/bright/accounts_test.exs
+++ test/bright/accounts_test.exs
@@ -87,7 +87,7 @@ defmodule Bright.AccountsTest do
 
     test "registers users with a hashed password" do
       email = unique_user_email()
-      {:ok, user} = Accounts.register_user(params_for(:user_with_password, email: email))
+      {:ok, user} = Accounts.register_user(params_for(:user_before_registration, email: email))
       assert user.email == email
       assert is_binary(user.hashed_password)
       assert is_nil(user.confirmed_at)
@@ -108,7 +108,7 @@ defmodule Bright.AccountsTest do
       changeset =
         Accounts.change_user_registration(
           %User{},
-          params_for(:user_with_password, email: email, password: password)
+          params_for(:user_before_registration, email: email, password: password)
         )
 
       assert changeset.valid?
