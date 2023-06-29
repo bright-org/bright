69b7ec6764132b319a576935f123f4c91fd569b7
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-06-30 00:30:01 +0900

    不要なコメントを削除

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,6 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-//import {Hooks} from './hooks/index.js'
 import * as hooks from "./hooks"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
