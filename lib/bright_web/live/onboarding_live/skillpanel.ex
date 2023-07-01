a063cb30a25127e5c9f2b215827d05471e90ce8f
nako <kanapo.kd@gmail.com>
2023-07-01 16:27:18 +0900

    オンボーディング やりたいこと選択画面追加

---
+++ lib/bright_web/live/onboarding_live/skillpanel.ex
@@ -0,0 +1,18 @@
+defmodule BrightWeb.OnboardingLive.Skillpanel do
+  use BrightWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  # @impl true
+  # def handle_params(params, _url, socket) do
+  #   {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  # end
+
+  # defp apply_action(socket, :skillpanel, _params) do
+  #   socket
+  #   |> assign(:page_title, "Listing Onboardings")
+  # end
+end
