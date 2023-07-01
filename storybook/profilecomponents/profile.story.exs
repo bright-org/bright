c1cc4935c51662ea51dcb22290d8d1d9b07b62df
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-02 07:41:19 +0900

    profile/1に変更

--- storybook/profilecomponents/profile.story.exs
+++ storybook/profilecomponents/profile.story.exs
@@ -1,7 +1,7 @@
 defmodule Storybook.Components.Profile do
   use PhoenixStorybook.Story, :component
 
-  def function, do: &Elixir.BrightWeb.ProfileComponents.profile/0
+  def function, do: &Elixir.BrightWeb.ProfileComponents.profile/1
 
   def variations do
     [
