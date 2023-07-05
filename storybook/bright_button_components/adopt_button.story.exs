f2b1786032fb3753c4acd19a800bdbfaae829b01
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-06 05:02:53 +0900

    storybookにボタンを追加

---
+++ storybook/bright_button_components/adopt_button.story.exs
@@ -0,0 +1,13 @@
+defmodule Storybook.Components.BrightButtonComponents do
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &Elixir.BrightWeb.BrightButtonComponents.adopt_button/1
+
+  def variations do
+    [
+      %Variation{
+        id: :default
+      }
+    ]
+  end
+end
