5563efb5cee6f8eb333854e09db7b68d66bd4499
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-06 22:52:17 +0900

    BrightWeb.MenuComponentsを削除

--- lib/bright_web/components/layouts.ex
+++ lib/bright_web/components/layouts.ex
@@ -2,8 +2,7 @@ defmodule BrightWeb.Layouts do
   @moduledoc false
 
   use BrightWeb, :html
-  import BrightWeb.MenuComponents
-  import BrightWeb.LayoutComponents
+    import BrightWeb.LayoutComponents
 
   embed_templates "layouts/*"
 end
