b87a3004236e273653423158e7c7d702222b466a
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-10 12:04:28 +0900

    not null制約とindexを修正

--- priv/repo/migrations/20230708100815_create_notifications.exs
+++ priv/repo/migrations/20230708100815_create_notifications.exs
@@ -3,17 +3,19 @@ defmodule Bright.Repo.Migrations.CreateNotifications do
 
   def change do
     create table(:notifications) do
-      add :from_user_id, references(:users, on_delete: :nothing)
-      add :to_user_id, references(:users, on_delete: :nothing)
-      add :icon_type, :string
-      add :message, :string
-      add :type, :string
+      add :from_user_id, references(:users, on_delete: :nothing), null: false
+      add :to_user_id, references(:users, on_delete: :nothing), null: false
+      add :icon_type, :string, null: false
+      add :message, :string, null: false
+      add :type, :string, null: false
       add :url, :string
       add :read_at, :naive_datetime
 
       timestamps()
     end
 
-    create index(:notifications, [:to_user_id, :type, :read_at])
+    create index(:notifications, [:to_user_id])
+    create index(:notifications, [:type])
+    create index(:notifications, [:read_at])
   end
 end
