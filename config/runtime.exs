2ddba28503411dec771bf01c74ef6698f3195b50
SAITO Kazuya <saito@smartalgorithm.co.jp>
2023-07-13 10:27:49 +0900

    chore: enable to send emails by SendGrid

--- config/runtime.exs
+++ config/runtime.exs
@@ -108,4 +108,7 @@ if config_env() == :prod do
   #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
   #
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+  config :bright, Bright.Mailer,
+    adapter: Swoosh.Adapters.Sendgrid,
+    api_key: System.get_env("SENDGRID_API_KEY")
 end
