e8a78821cfa370d40975346ce4fc414824ad98ac
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-13 10:38:26 +0900

    user_headerのみデザインを更新

--- lib/bright_web/components/layout_components.ex
+++ lib/bright_web/components/layout_components.ex
@@ -7,147 +7,113 @@ defmodule BrightWeb.LayoutComponents do
   @doc """
   Renders a User Header
 
+  # TODO 「4211a9a3ea766724d890e7e385b9057b4ddffc52」　「feat: フォームエラー、モーダル追加」　までマイページのみ部品デザイン更新
+
   ## Examples
       <.user_header />
   """
   def user_header(assigns) do
     ~H"""
-    <div class="w-full flex justify-between py-2.5 px-12 border-brightGray-100 border-b bg-brightGray-50">
+    <div class="w-full flex justify-between py-2.5 px-10 border-brightGray-100 border-b bg-white">
       <h4>マイページ</h4>
-      <div class="flex gap-x-6">
-        <button
-          type="button"
-          id="mega-menu-dropdown-button"
-          data-dropdown-toggle="mega-menu-dropdown"
-          class="text-black bg-brightGray-100 hover:bg-brightGray-200 rounded-full w-10 h-10 inline-flex items-center justify-center"
-        >
-          <span class="material-icons text-xs">search</span>
-        </button>
-        <button
-          type="button"
-          class="text-black bg-brightGray-100 hover:bg-brightGray-200 rounded-full w-10 h-10 inline-flex items-center justify-center relative"
-        >
-          <span class="material-icons text-xs">notifications_none</span>
-          <div class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-attention-600 rounded-full -top-0 -right-2">
-            1
-          </div>
-        </button>
-        <img
-          class="inline-block h-10 w-10 rounded-full"
-          src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
-        />
-        <button
-          type="button"
-          class="text-black bg-brightGray-100 hover:bg-brightGray-200 rounded-full w-10 h-10 inline-flex items-center justify-center"
-        >
-          <span class="material-icons text-xs">settings</span>
-        </button>
+      <div class="flex gap-x-5">
+          <button type="button"
+              class="text-white bg-brightGreen-300 px-4 inline-flex rounded-md text-sm items-center font-bold h-9 hover:opacity-70">
+              <span
+                  class="bg-white material-icons mr-1 !text-base !text-brightGreen-300 rounded-full h-6 w-6 !font-bold material-icons-outlined">sms</span>
+              カスタマーサクセスに連絡
+          </button>
+
+          <button type="button"
+              class="text-white bg-brightGreen-300 px-4 inline-flex rounded-md text-sm items-center font-bold h-9 hover:opacity-70">
+              <span
+                  class="bg-white material-icons mr-1 !text-base !text-brightGreen-300 rounded-full h-6 w-6 !font-bold">search</span>
+              スキル保有者を検索
+          </button>
+
+          <button type="button"
+              class="text-black bg-brightGray-50 hover:bg-brightGray-100 rounded-full w-10 h-10 inline-flex items-center justify-center relative">
+              <span class="material-icons">notifications_none</span>
+              <div
+                  class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-attention-600 rounded-full -top-0 -right-2">
+                  1
+              </div>
+          </button>
+          <button class="hover:opacity-70">
+              <img class="inline-block h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" />
+          </button>
       </div>
     </div>
-    <div
-      id="mega-menu-dropdown"
-      class="absolute hidden z-10 w-[1000px] !top-1 min-h-full grid-cols-2 text-sm bg-white shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700"
-    >
-      <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-        <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              About Us
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Library
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Resources
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Pro Version
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
-        <ul class="space-y-4">
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Blog
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Newsletter
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Playground
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              License
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="p-4">
-        <ul class="space-y-4">
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Support Center
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500"
-            >
-              Terms
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div id="mega-menu-dropdown"
+        class="absolute hidden z-10 w-[1000px] !top-1 min-h-full grid-cols-2 text-sm bg-white shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+            <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Library
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Resources
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Pro Version
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+            <ul class="space-y-4">
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Newsletter
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Playground
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        License
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="p-4">
+            <ul class="space-y-4">
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Contact Us
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Support Center
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                        Terms
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
     """
   end
