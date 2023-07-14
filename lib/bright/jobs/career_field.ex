d88ad0d7e867c883782067401fc972155ff6b09c
nako <kanapo.kd@gmail.com>
2023-07-14 22:42:49 +0900

    キャリアフィールドを扱うスキーマ UUID対応

--- lib/bright/jobs/career_field.ex
+++ lib/bright/jobs/career_field.ex
@@ -2,6 +2,8 @@ defmodule Bright.Jobs.CareerField do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, Ecto.ULID, autogenerate: true}
+
   schema "career_fields" do
     field :background_color, :string
     field :button_color, :string
