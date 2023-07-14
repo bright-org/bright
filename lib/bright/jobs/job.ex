5a63c948bb199349949d591fe6f7734f144c3b37
nako <kanapo.kd@gmail.com>
2023-07-14 23:14:56 +0900

    ジョブスキーマ更新

--- lib/bright/jobs/job.ex
+++ lib/bright/jobs/job.ex
@@ -1,11 +1,20 @@
 defmodule Bright.Jobs.Job do
+  @moduledoc """
+  ジョブを扱うスキーマ
+  """
   use Ecto.Schema
   import Ecto.Changeset
+
+  alias Bright.Jobs.CareerField
+
+  @primary_key {:id, Ecto.ULID, autogenerate: true}
+  @foreign_key_type Ecto.ULID
 
   schema "jobs" do
     field :name, :string
     field :position, :integer
-    field :career_fied_id, :id
+
+    belongs_to :career_fied, CareerField
 
     timestamps()
   end
