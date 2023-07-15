3c3c2773acb0260130006fbd542803dc2e717ba9
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-16 06:28:05 +0900

    「さまざまな人たちとの交流」をDBから取得する

--- lib/bright_web/components/communication_card_components.ex
+++ lib/bright_web/components/communication_card_components.ex
@@ -5,34 +5,25 @@ defmodule BrightWeb.CommunicationCardComponents do
   use Phoenix.Component
   import BrightWeb.TabComponents
 
+  @highlight_minutes 60 * 60 * 8
+
   @doc """
   Renders a Communication Card
 
-  ## Communications sample
-    [
-      %{icon_type: "pentagon", message: "中道洋介さんがスキルアップしました", time: 1, highlight: true},
-      %{icon_type: "pentagon", message: "清水太郎さんがスキルアップしました", time: 10, highlight: false}
-    ]
-
   ## Examples
-      <.communication_card communications={@communications} />
+      <.communication_card card={@card} />
   """
 
-  # TODO communicationsのdefault現在サンプルデータとして使ってます、DBのロジック作成後削除
-  attr :communications, :list,
-    default: [
-      %{icon_type: "pentagon", message: "中道洋介さんがスキルアップしました", time: 1, highlight: true},
-      %{icon_type: "pentagon", message: "清水太郎さんがスキルアップしました", time: 10, highlight: false}
-    ]
+  attr :card, :map
 
   def communication_card(assigns) do
     ~H"""
     <div>
       <h5>さまざまな人たちとの交流</h5>
-      <.tab tabs={["スキルアップ", "1on1のお誘い", "所属チームから", "「気になる」された", "運勢公式チーム発足"]}>
+      <.tab id="communication_card" tabs={["スキルアップ", "1on1のお誘い", "所属チームから", "「気になる」された", "運勢公式チーム発足"]} selected_tab={@card.selected_tab}>
         <ul class="flex gap-y-2.5 flex-col">
-          <%= for communication <- @communications do %>
-            <.communication_card_row communication={communication} />
+          <%= for notification <- @card.notifications do %>
+            <.communication_card_row notification={notification} />
           <% end %>
         </ul>
       </.tab>
@@ -43,24 +34,38 @@ defmodule BrightWeb.CommunicationCardComponents do
   attr :communication, :map, required: true
 
   def communication_card_row(assigns) do
-    style = highlight(assigns.communication.highlight) <> " font-bold pl-4 inline-block"
+    # TODO ↓contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
+    {:ok, inserted_at} = DateTime.from_naive(assigns.notification.inserted_at, "Etc/UTC")
+
+    minutes =
+      DateTime.diff(DateTime.utc_now(), inserted_at, :minute)
+      |> trunc()
+
+    time_text = if minutes < 60, do: "#{minutes}分前", else: "#{trunc(minutes / 60)}時間前"
+
+    style = highlight(minutes < @highlight_minutes) <> " font-bold pl-4 inline-block"
 
     assigns =
       assigns
       |> assign(:style, style)
+      |> assign(:time_text, time_text)
+
+    # TODO ↑contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
 
     ~H"""
     <li class="text-left flex items-center text-base hover:bg-brightGray-50 px-1">
       <span class="material-icons-outlined !text-sm !text-white bg-brightGreen-300 rounded-full !flex w-6 h-6 mr-2.5 !items-center !justify-center">
-        <%= @communication.icon_type %>
+        <%= @notification.icon_type %>
       </span>
-      <%= @communication.message %>
-      <span class={@style}><%= "#{@communication.time}時間前" %></span>
+      <%= @notification.message %>
+      <span class={@style}><%= @time_text %></span>
     </li>
     """
   end
 
-  # TODO 共通化対象
+  # TODO ↓contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
   defp highlight(true), do: "text-brightGreen-300"
   defp highlight(false), do: "text-brightGray-300"
+
+  # TODO ↑contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
 end
