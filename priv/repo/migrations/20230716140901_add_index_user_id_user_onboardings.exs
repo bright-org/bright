5dfc7a8c285a952cd309f9c8c44952f0d9483a13
nako <kanapo.kd@gmail.com>
2023-07-17 14:59:58 +0900

    Revert "オンボーディング user_onboardingテーブルからcompleted_atカラム削除"
    This reverts commit 8947611bb54f89b81e2e835c3450f1cfdae09283.

--- priv/repo/migrations/20230716140901_add_index_user_id_user_onboardings.exs
+++ priv/repo/migrations/20230716140901_add_index_user_id_user_onboardings.exs
@@ -2,13 +2,7 @@ defmodule Bright.Repo.Migrations.AddIndexUserIdUserOnboardings do
   use Ecto.Migration
 
   def change do
-    # user_onboardingsはオンボーディングは初回のみ登録可能なため、user_idはユニークにする
     drop_if_exists index(:user_onboardings, [:user_id])
     create unique_index(:user_onboardings, [:user_id])
-
-    # created_atと同じ値が入るため不要なのでカラム削除
-    alter table("user_onboardings") do
-      remove_if_exists :completed_at, :naive_datetime
-    end
   end
 end
