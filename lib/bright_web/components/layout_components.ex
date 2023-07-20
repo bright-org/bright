9047258630018f206df22b4b990a502c1d26eaee
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-20 15:45:19 +0900

    メニューからマイページのリンクを削除し、ロゴをクリック時にマイページに移動する対応

--- lib/bright_web/components/layout_components.ex
+++ lib/bright_web/components/layout_components.ex
@@ -178,7 +178,7 @@ defmodule BrightWeb.LayoutComponents do
     <aside
     class="flex bg-brightGray-900 min-h-screen flex-col w-[200px] pt-3"
     >
-      <img src="/images/common/logo.svg" width="163px" class="ml-4" />
+      <a href="/mypage"><img src="/images/common/logo.svg" width="163px" class="ml-4" /></a>
       <ul class="grid pt-2">
         <%= for {title, path} <- links() do %>
           <li>
@@ -192,7 +192,6 @@ defmodule BrightWeb.LayoutComponents do
 
   def links() do
     [
-      {"マイページ", "/mypage"},
       {"スキルを選ぶ", "/onboardings"},
       {"成長を見る・比較する", "/"},
       {"スキルパネルを入力", "/panels/dummy_id/graph"},
