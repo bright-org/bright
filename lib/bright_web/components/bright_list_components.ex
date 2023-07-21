66ffea477aa1869ad02d31064d82f195567ef974
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-21 10:55:45 +0900

    Communication Card修正

--- lib/bright_web/components/bright_list_components.ex
+++ lib/bright_web/components/bright_list_components.ex
@@ -39,6 +39,38 @@ defmodule BrightWeb.BrightListComponents do
     """
   end
 
+  attr :notification, :map, required: true
+
+  def communication_card_row(assigns) do
+    # TODO ↓contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
+    {:ok, inserted_at} = DateTime.from_naive(assigns.notification.inserted_at, "Etc/UTC")
+
+    minutes =
+      DateTime.diff(DateTime.utc_now(), inserted_at, :minute)
+      |> trunc()
+
+    time_text = if minutes < 60, do: "#{minutes}分前", else: "#{trunc(minutes / 60)}時間前"
+
+    style = highlight(minutes < @highlight_minutes) <> " font-bold pl-4 inline-block"
+
+    assigns =
+      assigns
+      |> assign(:style, style)
+      |> assign(:time_text, time_text)
+
+    # TODO ↑contact_card_rowのソースと同じ　他のカードと考えて最終的には共通をすること
+
+    ~H"""
+    <li class="text-left flex items-center text-base hover:bg-brightGray-50 px-1">
+      <span class="material-icons-outlined !text-sm !text-white bg-brightGreen-300 rounded-full !flex w-6 h-6 mr-2.5 !items-center !justify-center">
+        <%= @notification.icon_type %>
+      </span>
+      <%= @notification.message %>
+      <span class={@style}><%= @time_text %></span>
+    </li>
+    """
+  end
+
   defp highlight(true), do: "text-brightGreen-300"
   defp highlight(false), do: "text-brightGray-300"
 end
