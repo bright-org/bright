ca2caec3a318dbd76ad9f22f591d0512db6b6915
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-24 14:43:19 +0900

    不要になったボタンのstorybookを削除

--- storybook/bright_button_components/profile_button.story.exs
+++ storybook/bright_button_components/profile_button.story.exs
@@ -1,7 +1,7 @@
 defmodule Storybook.Components.BrightButtonComponents do
   use PhoenixStorybook.Story, :component
 
-  def function, do: &Elixir.BrightWeb.BrightButtonComponents.bright_button/1
+  def function, do: &Elixir.BrightWeb.BrightButtonComponents.profile_button/1
 
   def variations do
     [
