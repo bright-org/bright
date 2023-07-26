b901e3673a09d6673101d7a1ece3074a41ae869a
koyo-miyamura <nexus9071@gmail.com>
2023-07-26 09:02:23 +0900

    Add build(:user)

--- test/factories/user_token_factory.ex
+++ test/factories/user_token_factory.ex
@@ -6,7 +6,9 @@ defmodule Bright.UserTokenFactory do
   defmacro __using__(_opts) do
     quote do
       def user_token_factory do
-        %Bright.Accounts.UserToken{}
+        %Bright.Accounts.UserToken{
+          user: build(:user)
+        }
       end
 
       def extract_user_token(fun) do
