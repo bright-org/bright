265d3c41bd54fc2f596f39a70cd4e59d02a1a13d
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-26 14:35:16 +0900

    変数名修正

--- assets/js/hooks/SkillGem.js
+++ assets/js/hooks/SkillGem.js
@@ -96,18 +96,18 @@ const beforeDatasetsDraw = (chart) => {
   const colorTheme = chart.canvas.parentNode.dataset.colorTheme
   const myselfData = chart.data.datasets[0].data
   const diffData = chart.data.datasets[1] !== undefined ? chart.data.datasets[1].data : []
-  const myColor = getColorPattern(myselfData.length, ["#72EAD9C0", "#3CC0A8C0", "#1DA091C0"])
+  const myselfColor = getColorPattern(myselfData.length, ["#72EAD9C0", "#3CC0A8C0", "#1DA091C0"])
   const otherColor = getColorPattern(myselfData.length, ["#E4BDE9AA", "#C063CDAA", "#9510B1AA"])
   const pastColor = getColorPattern(myselfData.length,   ["#FFFFFF99", "#FFFFFF55", "#FFFFFF55"])
   const diffColor = colorTheme === 'myself' ? pastColor : otherColor
   const isLink = JSON.parse(context.canvas.parentElement.dataset.displayLink)
 
   if (colorTheme === 'myself') {
-    fillSurfaces(chart, myselfData, myColor)
+    fillSurfaces(chart, myselfData, myselfColor)
     fillSurfaces(chart, diffData, diffColor)
   } else {
     fillSurfaces(chart, diffData, diffColor)
-    fillSurfaces(chart, myselfData, myColor)
+    fillSurfaces(chart, myselfData, myselfColor)
   }
 
   for (let i = 1; i < 5; i++) {
