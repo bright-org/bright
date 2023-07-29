7ef90d4727d3501fc3e131a86df78ace311d6014
nako <kanapo.kd@gmail.com>
2023-07-29 17:22:38 +0900

    関数と変数を区別できるよう修正

--- lib/bright_web/live/admin/career_want_job_live/index.ex
+++ lib/bright_web/live/admin/career_want_job_live/index.ex
@@ -21,8 +21,8 @@ defmodule BrightWeb.Admin.CareerWantJobLive.Index do
   end
 
   defp apply_action(socket, :new, _params) do
-    career_wants_options = Jobs.list_career_wants() |> map_to_select_option
-    jobs_options = Jobs.list_jobs() |> map_to_select_option
+    career_wants_options = Jobs.list_career_wants() |> map_to_select_option()
+    jobs_options = Jobs.list_jobs() |> map_to_select_option()
 
     socket
     |> assign(:page_title, "New Career want job")
