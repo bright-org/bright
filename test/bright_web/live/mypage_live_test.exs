8a24189c4137653ac5e21c46c0709d9042f352c9
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-07-30 13:23:25 +0900

    各カードの見出しテスト

--- test/bright_web/live/mypage_live_test.exs
+++ test/bright_web/live/mypage_live_test.exs
@@ -2,15 +2,30 @@ defmodule BrightWeb.MypageLiveTest do
   use BrightWeb.ConnCase
 
   import Phoenix.LiveViewTest
-  import Bright.Factory
 
   describe "Index" do
     setup [:register_and_log_in_user]
 
-    test "lists all mypages", %{conn: conn} do
-      {:ok, _index_live, html} = live(conn, ~p"/mypage")
+    test "view mypages", %{conn: conn} do
+      {:ok, index_live, html} = live(conn, ~p"/mypage")
 
-      assert html =~ "マイページ"
+     assert html =~ "マイページ"
+
+      assert index_live |> has_element?("h5", "重量な連絡")
+      assert index_live |> has_element?("li a", "チーム招待")
+
+      assert index_live |> has_element?("h5", "保有スキル（ジェムをクリックすると成長グラフが見れます）")
+      assert index_live |> has_element?("li a", "エンジニア")
+
+      assert index_live |> has_element?("h5", "さまざまな人たちとの交流")
+      assert index_live |> has_element?("li a", "スキルアップ")
+
+      assert index_live |> has_element?("h5", "関わっているチーム")
+      assert index_live |> has_element?("li a", "所属チーム")
+
+      assert index_live |> has_element?("h5", "関わっているユーザー")
+      assert index_live |> has_element?("li a", "気になる人")
+
     end
   end
 end
