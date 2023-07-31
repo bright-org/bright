e88dfca0fd047ba9ab85e791e2652cd2276ad655
koyo-miyamura <nexus9071@gmail.com>
2023-08-01 00:08:33 +0900

    OAuthController の実装

--- lib/bright_web/controllers/oauth_controller.ex
+++ lib/bright_web/controllers/oauth_controller.ex
@@ -8,6 +8,9 @@ defmodule BrightWeb.OAuthController do
 
   require Logger
   alias Ueberauth.Strategy.Helpers
+  alias BrightWeb.UserAuth
+  alias Bright.Accounts
+  alias Bright.Accounts.User
 
   def request(conn, _params) do
     redirect(conn, to: Helpers.request_url(conn))
@@ -21,11 +24,42 @@ defmodule BrightWeb.OAuthController do
     |> redirect(to: ~p"/users/log_in")
   end
 
-  def callback(%{assigns: %{ueberauth_auth: %Ueberauth.Auth{} = auth}} = conn, _params) do
-    Logger.info(auth)
+  def callback(
+        %{
+          assigns: %{
+            ueberauth_auth: %Ueberauth.Auth{
+              info: %Ueberauth.Auth.Info{name: name, email: email},
+              provider: provider,
+              uid: identifier
+            }
+          }
+        } = conn,
+        _params
+      ) do
+    user = Accounts.get_user_by_provider_and_identifier(provider, identifier)
 
-    conn
-    |> put_flash(:info, "Succeed to authenticate!")
-    |> redirect(to: "/")
+    case Accounts.get_user_by_provider_and_identifier(provider, identifier) do
+      %User{confirmed_at: nil} ->
+        conn
+        |> put_flash(:error, "メールアドレス未確認ユーザーです。メールを確認して確認済みにしてください。")
+        |> redirect(to: ~p"/users/log_in")
+
+      %User{} = user ->
+        conn
+        |> put_flash(:info, "ログインしました")
+        |> UserAuth.log_in_user(user)
+
+      _ ->
+        token =
+          Accounts.generate_social_identifier_token(%{
+            name: name,
+            email: email,
+            provider: provider,
+            identifier: identifier
+          })
+
+        conn
+        |> redirect(to: ~p"/users/register_social_account/#{token}")
+    end
   end
 end
