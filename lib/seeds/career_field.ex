37fe1055b6631040e5d611d002107a12c4cd34f3
thehaigo <sirannha@gmail.com>
2023-08-03 12:15:11 +0900

    seed作成

---
+++ lib/seeds/career_field.ex
@@ -0,0 +1,22 @@
+defmodule Bright.Seeds.CareerField do
+  alias Bright.Repo
+  alias Bright.Jobs
+  alias Bright.Jobs.CareerField
+
+  @data [
+    %{name_en: "enginner", name_ja: "エンジニア", position: 1},
+    %{name_en: "infra", name_ja: "インフラ", position: 2},
+    %{name_en: "designer", name_ja: "デザイナー", position: 3},
+    %{name_en: "marketer", name_ja: "マーケッター", position: 4}
+  ]
+
+  def insert() do
+    CareerField
+    |> Repo.all()
+    |> Enum.each(&Repo.delete(&1))
+
+    Enum.each(@data, fn c ->
+      Jobs.create_career_field(c)
+    end)
+  end
+end
