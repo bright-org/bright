396e49a8e6dc8cc7ead9e66bf4cf29cdced88452
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-04 15:58:31 +0900

    Migration修正

--- priv/repo/migrations/20230804064749_create_user_skill_panels.exs
+++ priv/repo/migrations/20230804064749_create_user_skill_panels.exs
@@ -3,10 +3,12 @@ defmodule Bright.Repo.Migrations.CreateUserSkillPanels do
 
   def change do
     create table(:user_skill_panels) do
-      add :user_id, :uuid
-      add :skill_panel_id, :uuid
+      add :user_id, references(:users, on_delete: :nothing), null: false
+      add :skill_panel_id, references(:skill_panels, on_delete: :nothing), null: false
 
       timestamps()
     end
+
+    create unique_index(:user_skill_panels, [:user_id, :skill_panel_id])
   end
 end
