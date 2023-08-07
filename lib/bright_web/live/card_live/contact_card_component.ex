cadf86efbab835c4fd4834a74c0541978d5e858c
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-08 07:06:37 +0900

    重要な連絡0件対応

--- lib/bright_web/live/card_live/contact_card_component.ex
+++ lib/bright_web/live/card_live/contact_card_component.ex
@@ -30,7 +30,14 @@ defmodule BrightWeb.CardLive.ContactCardComponent do
         total_pages={@card.total_pages}
         target={@myself}
       >
-        <div class="pt-4 px-8 h-[216px]">
+        <div class="pt-4 px-6 min-h-[216px]">
+          <ul :if={Enum.count(@card.notifications) == 0}  class="flex gap-y-2.5 flex-col">
+            <li class="flex">
+              <div class="text-left flex items-center text-base px-1 py-1 flex-1 mr-2">
+                <%= Enum.into(@tabs, %{}) |> Map.get(@card.selected_tab) %>はありません
+              </div>
+            </li>
+          </ul>
           <ul class="flex gap-y-2.5 flex-col">
             <%= for notification <- @card.notifications do %>
               <.card_row type="contact" notification={notification} />
