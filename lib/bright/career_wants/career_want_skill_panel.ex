6d73d12113977264264e9c4e29153158cb999d89
thehaigo <sirannha@gmail.com>
2023-08-09 21:17:23 +0900

    create career_want_skill_panels

---
+++ lib/bright/career_wants/career_want_skill_panel.ex
@@ -0,0 +1,24 @@
+defmodule Bright.CareerWants.CareerWantSkillPanel do
+  @moduledoc """
+  やりたいこととスキルパネルを関連づけるスキーマ。
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.ULID, autogenerate: true}
+  @foreign_key_type Ecto.ULID
+
+  schema "career_want_skill_panels" do
+    belongs_to :career_want, Bright.CareerWants.CareerWant
+    belongs_to :skill_panel, Bright.SkillPanels.SkillPanel
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(career_want_skill_panel, attrs) do
+    career_want_skill_panel
+    |> cast(attrs, [:career_want_id, :skill_panel_id])
+    |> validate_required([:skill_panel_id])
+  end
+end
