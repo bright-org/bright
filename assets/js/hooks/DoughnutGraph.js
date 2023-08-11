2e09905e2a8922ab9feae1d776880fc63dc0def1
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-11 17:24:42 +0900

    hooks updated試作

--- assets/js/hooks/DoughnutGraph.js
+++ assets/js/hooks/DoughnutGraph.js
@@ -2,7 +2,7 @@ import { Chart } from 'chart.js/auto'
 const circleMarkCokir = '#0EA895'
 const triangleMarkCokir = '#14D6BE'
 const noneMarkCokir = '#EFF0F0'
-
+const myDoughnut = null
 const createDoughnutData = (data) => {
   return {
     datasets: [
@@ -44,14 +44,27 @@ const createChartFromJSON = (data) => {
 
 export const DoughnutGraph = {
   mounted() {
+    console.log("mounted")
     const element = this.el
     const dataset = element.dataset
     const data = JSON.parse(dataset.data)
 
     const ctx = document.querySelector('#' + element.id + ' canvas')
-    const myChart = new Chart(ctx, createChartFromJSON(data))
-    myChart.canvas.parentNode.style.height = '80px'
-    myChart.canvas.parentNode.style.width = '80px'
+    myDoughnut = new Chart(ctx, createChartFromJSON(data))
+    myDoughnut.canvas.parentNode.style.height = '80px'
+    myDoughnut.canvas.parentNode.style.width = '80px'
+    console.log(window)
+  },
+  updated() {
+    if(myDoughnut != null) myDoughnut.destroy()
+    const element = this.el
+    const dataset = element.dataset
+    const data = JSON.parse(dataset.data)
+
+    const ctx = document.querySelector('#' + element.id + ' canvas')
+    myDoughnut = new Chart(ctx, createChartFromJSON(data))
+    myDoughnut.canvas.parentNode.style.height = '80px'
+    myDoughnut.canvas.parentNode.style.width = '80px'
 
   }
 }
