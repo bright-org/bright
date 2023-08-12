bd53654a6d66b32fced47429ee252452c61ae235
thehaigo <sirannha@gmail.com>
2023-08-12 15:49:31 +0900

    アコーディオンをindexに移動

--- lib/bright_web/live/onboarding_live/index.ex
+++ lib/bright_web/live/onboarding_live/index.ex
@@ -3,9 +3,18 @@ defmodule BrightWeb.OnboardingLive.Index do
 
   alias Bright.Onboardings
 
+  @panels %{
+    "want_todo_panel" => :open_want_todo,
+    "wants_job_panel" => :open_wants_job
+  }
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :page_title, "Listing Onboardings")}
+    socket
+    |> assign(:page_title, "オンボーディング")
+    |> assign(:open_want_todo, false)
+    |> assign(:open_wants_job, false)
+    |> then(&{:ok, &1})
   end
 
   @impl true
@@ -29,4 +38,35 @@ defmodule BrightWeb.OnboardingLive.Index do
         {:noreply, assign(socket, changeset)}
     end
   end
+
+  @impl true
+  def handle_event("toggle_panel", %{"panel" => panel}, socket) do
+    IO.inspect(hide_panel(panel))
+
+    socket
+    |> assign(@panels[panel], !Map.get(socket.assigns, @panels[panel]))
+    |> assign(@panels[hide_panel(panel)], false)
+    |> then(&{:noreply, &1})
+  end
+
+  defp toggle(js \\ %JS{}, id) do
+    js
+    |> JS.push("toggle_panel", value: %{panel: id})
+    |> JS.toggle(to: "##{id}")
+    |> JS.hide(to: "##{hide_panel(id)}")
+  end
+
+  defp hide_panel(id) do
+    IO.inspect(id)
+
+    Map.keys(@panels)
+    |> Enum.reject(&(&1 == id))
+    |> List.first()
+  end
+
+  defp close(),
+    do: "before:-mt-2 before:rotate-225"
+
+  defp open(),
+    do: "rounded-bl-none rounded-br-none before:-mt-0.5 before:rotate-45"
 end
