92972954303f45786b8c2c6fbbba57dff1e3281d
koyo-miyamura <nexus9071@gmail.com>
2023-08-17 00:52:14 +0900

    ログアウトボタンの反映

--- lib/bright_web/components/bright_button_components.ex
+++ lib/bright_web/components/bright_button_components.ex
@@ -171,15 +171,16 @@ defmodule BrightWeb.BrightButtonComponents do
 
   def logout_button(assigns) do
     ~H"""
-        <.link
-          href="/users/log_out"
-          method="delete"
-          class="hover:opacity-70"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-10 w-10 stroke-red-400">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-          </svg>
-        </.link>
+    <.link
+      href="/users/log_out"
+      method="delete"
+      >
+      <button
+        class="rounded-full border border-brightGray-600 h-10 w-10 flex items-center justify-center"
+      >
+        <span class="material-icons text-brightGray-600">logout</span>
+      </button>
+    </.link>
     """
   end
 end
