8219afe81c356676dc62d839ad5963cc2558aef4
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-17 15:48:16 +0900

    ProfileHelperを作成

---
+++ lib/bright_web/live/mypage_live/profile_helper.ex
@@ -0,0 +1,54 @@
+defmodule BrightWeb.MypageLive.ProfileHelper do
+  @moduledoc """
+  Profile取得共通処理
+  """
+  use Phoenix.Component
+  alias Bright.Accounts
+  alias Bright.Repo
+  alias Bright.Utils.Aes.Aes128
+  alias Bright.Accounts.User
+
+  # TODO: プロフィール読み込み共通化対象
+  def assign_display_user(socket, %{"user_name" => user_name}) do
+    # TODO: チームに所属のチェックを実装すること
+    user =
+      Accounts.get_user_by_name_or_email(user_name)
+      |> Repo.preload(:user_profile)
+
+    socket
+    |> assign(:is_anonymous, false)
+    |> assign(:display_user, user)
+  end
+
+  def assign_display_user(socket, %{"user_name_crypted" => user_name_crypted}) do
+    user =
+      decrypt_user_name(user_name_crypted)
+      |> Accounts.get_user_by_name_or_email()
+
+    display_user =
+      %User{}
+      |> Map.put(:user_profile, %Bright.UserProfiles.UserProfile{})
+      |> Map.put(:id, user.id)
+
+    socket
+    |> assign(:is_anonymous, true)
+    |> assign(:display_user, display_user)
+  end
+
+  def assign_display_user(socket, _params) do
+    socket
+    |> assign(:is_anonymous, false)
+    |> assign(:display_user, socket.assigns.current_user)
+  end
+
+  def encrypt_user_name(%User{} = user) do
+    date_time = user.inserted_at |> NaiveDateTime.to_string()
+    Aes128.encrypt("#{user.name},#{date_time}")
+  end
+
+  def decrypt_user_name(ciphertext) do
+    Aes128.decrypt(ciphertext)
+    |> String.split(",")
+    |> List.first()
+  end
+end
