621c58d6afa09b8df3fe1facbe5798f3be9f6ccd
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-18 06:55:54 +0900

    タイムラインとスキルジェムを連携

--- lib/bright_web/live/chart_live/skill_gem_component.ex
+++ lib/bright_web/live/chart_live/skill_gem_component.ex
@@ -5,6 +5,7 @@ defmodule BrightWeb.ChartLive.SkillGemComponent do
   use BrightWeb, :live_component
   import BrightWeb.ChartComponents
   alias Bright.SkillScores
+  alias Bright.HistoricalSkillUnitScore
 
   @impl true
   def render(assigns) do
@@ -24,7 +25,11 @@ defmodule BrightWeb.ChartLive.SkillGemComponent do
       socket
       |> assign(assigns)
 
-    skill_gem = SkillScores.get_skill_gem(assigns.user_id, assigns.skill_panel_id, assigns.class)
+    select_label = assigns[:select_label] || "now"
+
+    skill_gem =
+      get_skill_gem(assigns.user_id, assigns.skill_panel_id, assigns.class, select_label)
+
     skill_gem_data = [skill_gem |> Enum.map(fn x -> x.percentage end)]
     skill_gem_labels = skill_gem |> Enum.map(fn x -> x.name end)
 
@@ -34,5 +39,25 @@ defmodule BrightWeb.ChartLive.SkillGemComponent do
       |> assign(:skill_gem_labels, skill_gem_labels)
 
     {:ok, socket}
+  end
+
+  def get_skill_gem(user_id, skill_panel_id, class, select_label) when select_label == "now",
+    do: SkillScores.get_skill_gem(user_id, skill_panel_id, class)
+
+  def get_skill_gem(user_id, skill_panel_id, class, select_label),
+    do:
+      HistoricalSkillUnitScore.get_historical_skill_gem(
+        user_id,
+        skill_panel_id,
+        class,
+        label_to_date(select_label)
+      )
+
+  defp label_to_date(date) do
+    "#{date}.1"
+    |> String.split(".")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+    |> Date.from_erl!()
   end
 end
