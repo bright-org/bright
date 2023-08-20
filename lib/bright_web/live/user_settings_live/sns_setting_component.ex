ba6131a875ca8773b2d8b65b27ddb58136760f07
koyo-miyamura <nexus9071@gmail.com>
2023-08-21 01:59:43 +0900

    SnsSettingComponentLiveTest のテスト追加

--- lib/bright_web/live/user_settings_live/sns_setting_component.ex
+++ lib/bright_web/live/user_settings_live/sns_setting_component.ex
@@ -10,7 +10,7 @@ defmodule BrightWeb.UserSettingsLive.SnsSettingComponent do
     ~H"""
     <li class="block">
       <div class="flex flex-col mt-8">
-        <div class="flex items-center mb-4 text-left" :for={linked_user_social_auth <- @linked_user_social_auths}>
+        <div id="user_settings_sns_linked_provider" class="flex items-center mb-4 text-left" :for={linked_user_social_auth <- @linked_user_social_auths}>
           <div class="w-full">
             <BrightWeb.UserAuthComponents.social_auth_button method="delete" href={~p"/auth/#{linked_user_social_auth.provider}"} variant={to_string(linked_user_social_auth.provider)}>
               <%= UserSocialAuth.provider_name(linked_user_social_auth.provider) %>と連携解除する
@@ -18,7 +18,7 @@ defmodule BrightWeb.UserSettingsLive.SnsSettingComponent do
             <span class="ml-4"><%= linked_user_social_auth.display_name %>で連携中</span>
           </div>
         </div>
-        <div class="flex items-center mb-4 text-left" :for={unlink_provider <- @unlink_providers}>
+        <div id="user_settings_sns_unlinked_provider" class="flex items-center mb-4 text-left" :for={unlink_provider <- @unlink_providers}>
           <div class="w-full">
             <BrightWeb.UserAuthComponents.social_auth_button href={if unlink_provider in not_implemented_providers(), do: "#", else: ~p"/auth/#{unlink_provider}"} variant={to_string(unlink_provider)}>
               <%= UserSocialAuth.provider_name(unlink_provider) %>と連携する
