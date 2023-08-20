3bacbdd788f858a0ac1f09def9a593409a65c255
koyo-miyamura <nexus9071@gmail.com>
2023-08-20 18:01:27 +0900

    ログイン中に確認メールにアクセスされるとログアウト処理を行わずにログインできてしまうので修正

--- lib/bright_web/controllers/user_confirmation_controller.ex
+++ lib/bright_web/controllers/user_confirmation_controller.ex
@@ -12,16 +12,9 @@ defmodule BrightWeb.UserConfirmationController do
         |> UserAuth.log_in_user(user)
 
       :error ->
-        case conn.assigns do
-          %{current_user: current_user} when not is_nil(current_user.confirmed_at) ->
-            conn
-            |> redirect(to: UserAuth.log_in_redirect_path(current_user))
-
-          %{} ->
-            conn
-            |> put_flash(:error, "リンクが無効であるか期限が切れています")
-            |> redirect(to: ~p"/users/log_in")
-        end
+        conn
+        |> put_flash(:error, "リンクが無効であるか期限が切れています")
+        |> redirect(to: ~p"/users/log_in")
     end
   end
 end
