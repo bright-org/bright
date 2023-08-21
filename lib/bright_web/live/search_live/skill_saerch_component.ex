e83005d4e05a8481d3d8ea04ba6c78cba9ee6f70
thehaigo <sirannha@gmail.com>
2023-08-21 15:42:40 +0900

    skill_search modal 作成

---
+++ lib/bright_web/live/search_live/skill_saerch_component.ex
@@ -0,0 +1,59 @@
+defmodule BrightWeb.SearchLive.SkillSearchComponent do
+  use BrightWeb, :live_component
+
+  alias BrightWeb.BrightCoreComponents, as: BrightCore
+  import BrightWeb.TabComponents
+
+  @tabs [
+    {"user", "ユーザー検索"}
+    # αでは落とす
+    # {"team", "チーム検索"}
+  ]
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div id="skill_search_modal" class="hidden">
+      <BrightCore.flash_group flash={@modal_flash} />
+      <div class="bg-zinc-50/90 fixed inset-0 transition-opacity" />
+      <div class="fixed inset-0 overflow-y-auto">
+        <section id="user_search" class="absolute bg-white min-h-[600px] p-4 right-0 shadow text-sm top-[60px] w-[800px] z-20">
+          <div class="w-full mb-4">
+          <button class="absolute top-4 right-8">
+            <span
+              class="material-icons text-white !text-sm bg-base rounded-full !inline-flex w-4 h-4 !items-center !justify-center"
+              phx-click={JS.hide(to: "#skill_search_modal")}
+            >close</span>
+          </button>
+          </div>
+          <.tab
+            id="skill_search_tab"
+            tabs={@tabs}
+            hidden_footer={true}
+            selected_tab={@selected_tab}
+            target={@myself}
+          >
+          </.tab>
+        </section>
+      </div>
+    </div>
+    """
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    socket
+    |> assign(assigns)
+    |> assign(:tabs, @tabs)
+    |> assign(:selected_tab, "user")
+    |> assign(:modal_flash, Map.get(assigns, :modal_flash, %{}))
+    |> then(&{:ok, &1})
+  end
+
+  @impl true
+  def handle_event("tab_click", %{"tab_name" => tab_name}, socket) do
+    socket
+    |> assign(:selected_tab, tab_name)
+    |> then(&{:noreply, &1})
+  end
+end
