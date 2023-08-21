adb732981132b3f3d652d0c1ab1d488042a146df
koyo-miyamura <nexus9071@gmail.com>
2023-08-22 03:56:57 +0900

    ユーザー設定: 一般の実装、ファイルアップロードを除く

--- lib/bright_web/live/user_settings_live/general_setting_component.ex
+++ lib/bright_web/live/user_settings_live/general_setting_component.ex
@@ -1,63 +1,123 @@
 defmodule BrightWeb.UserSettingsLive.GeneralSettingComponent do
   use BrightWeb, :live_component
 
+  alias Bright.Accounts
+  alias BrightWeb.BrightCoreComponents, as: BrightCore
+
   @impl true
   def render(assigns) do
     ~H"""
     <li class="block">
-      <div class="border-b border-brightGray-200 flex flex-wrap text-left">
-        <div class="w-1/2">
-          <label class="border-b border-brightGray-200 flex items-center py-4">
-            <span class="w-32">ハンドル名</span>
-            <input type="text" size="20" name="hn" class="border border-brightGray-200 px-2 py-1 rounded w-60">
-          </label>
-          <label class="border-b border-brightGray-200 flex items-center py-4">
-            <span class="w-32">称号</span>
-            <input type="text" size="20" name="title" class="border border-brightGray-200 px-2 py-1 rounded w-60">
-          </label>
+      <.form id="general_setting_form" :let={f} for={@form} phx-target={@myself} phx-submit="save" phx-change="validate">
+        <div class="border-b border-brightGray-200 flex flex-wrap text-left">
+          <div class="w-1/2">
+            <label class="border-b border-brightGray-200 flex items-center py-4">
+              <span class="w-32">ハンドル名</span>
+              <BrightCore.input field={f[:name]} type="text" size="20" input_class="px-2 py-1 rounded w-60" />
+            </label>
+            <label class="border-b border-brightGray-200 flex items-center py-4">
+              <span class="w-32">称号</span>
+              <.inputs_for :let={ff} field={@form[:user_profile]}>
+                <BrightCore.input field={ff[:title]} type="text" size="20" input_class="px-2 py-1 rounded w-60" />
+              </.inputs_for>
+            </label>
 
-          <label class="flex items-center pt-4 pb-2">
-            <span class="w-32">GitHub</span>
-            <input type="text" size="20" name="url_github" class="border border-brightGray-200 px-2 py-1 rounded w-60">
-          </label>
+            <label class="flex items-center pt-4 pb-2">
+              <span class="w-32">GitHub</span>
+              <.inputs_for :let={ff} field={@form[:user_profile]}>
+                <BrightCore.input field={ff[:github_url]} type="text" size="20" input_class="px-2 py-1 rounded w-60 placeholder-brightGray-100" placeholder="https://github.com/" />
+              </.inputs_for>
+            </label>
 
-          <label class="flex items-center py-2">
-            <span class="w-32">Twitter</span>
-            <input type="text" size="20" name="url_twitter" class="border border-brightGray-200 px-2 py-1 rounded w-60">
-          </label>
+            <label class="flex items-center py-2">
+              <span class="w-32">Twitter</span>
+              <.inputs_for :let={ff} field={@form[:user_profile]}>
+                <BrightCore.input field={ff[:twitter_url]} type="text" size="20" input_class="px-2 py-1 rounded w-60 placeholder-brightGray-100" placeholder="https://twitter.com/" />
+              </.inputs_for>
+            </label>
 
-          <label class="flex items-center pt-2 pb-4">
-            <span class="w-32">Facebook</span>
-            <input type="text" size="20" name="url_facebook" class="border border-brightGray-200 px-2 py-1 rounded w-60">
+            <label class="flex items-center pt-2 pb-4">
+              <span class="w-32">Facebook</span>
+              <.inputs_for :let={ff} field={@form[:user_profile]}>
+                <BrightCore.input field={ff[:facebook_url]} type="text" size="20" input_class="px-2 py-1 rounded w-60 placeholder-brightGray-100" placeholder="https://www.facebook.com/" />
+              </.inputs_for>
+            </label>
+          </div>
+
+          <div class="relative py-4 w-1/2">
+            <p>アイコン</p>
+            <label for={@uploads.icon.ref} class={[
+              "absolute bg-20 block cursor-pointer h-20 left-1/2 -ml-10 -mt-10 top-1/2 w-20",
+              length(@uploads.icon.entries) == 0 && "bg-bgAddAvatar"
+              ]}>
+              <.live_file_input upload={@uploads.icon} class="hidden" />
+
+              <%= for entry <- @uploads.icon.entries do %>
+                <.live_img_preview entry={entry} class="cursor-pointer rounded-full" />
+              <% end %>
+            </label>
+          </div>
+        </div>
+
+        <div class="text-left">
+          <label class="flex py-4 w-full">
+            <span class="py-1 w-32">自己紹介</span>
+            <.inputs_for :let={ff} field={@form[:user_profile]}>
+              <BrightCore.input type="textarea" field={ff[:detail]} div_class="w-4/5" input_class="w-4/5" rows="3" cols="20" />
+            </.inputs_for>
           </label>
         </div>
 
-        <div class="relative py-4 w-1/2">
-          <p>アイコン</p>
-          <lavel class="absolute bg-bgAddAvatar bg-20 block cursor-pointer h-20 left-1/2 -ml-10 -mt-10 top-1/2 w-20">
-            <input type="file" name="avatar" class="cursor-pointer h-20 opacity-0 w-20">
-          </lavel>
+        <div class="flex mt-8 relative">
+          <button type="submit" class="bg-brightGray-900 border block border-solid border-brightGray-900 cursor-pointer font-bold mx-auto px-4 py-2 rounded select-none text-center text-white w-80 hover:opacity-50">保存する</button>
         </div>
-      </div>
-
-      <div class="text-left">
-        <label class="flex py-4 w-full">
-          <span class="py-1 w-32">自己紹介</span>
-          <textarea rows="3" cols="20" name="introduction" class="border border-brightGray-200 px-2 py-1 rounded w-4/5"></textarea>
-        </label>
-      </div>
-
-      <div class="flex mt-8 relative">
-        <a class="bg-brightGray-900 border block border-solid border-brightGray-900 cursor-pointer font-bold mx-auto px-4 py-2 rounded select-none text-center text-white w-80 hover:opacity-50">保存する</a>
-      </div>
+      </.form>
     </li>
     """
   end
 
+  # TODO
+  # アイコン画像のバリデーション
+  # アイコン画像の保存処理
   @impl true
-  def update(assigns, socket) do
+  def update(%{user: user} = assigns, socket) do
+    changeset =
+      user
+      |> Bright.Repo.preload(:user_profile)
+      |> Accounts.change_user_with_user_profile()
+
     {:ok,
      socket
-     |> assign(assigns)}
+     |> assign(assigns)
+     |> allow_upload(:icon, accept: ~w(.jpg .jpeg .png), max_file_size: 2_000_000)
+     |> assign_form(changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset =
+      socket.assigns.user
+      |> Bright.Repo.preload(:user_profile)
+      |> Accounts.change_user_with_user_profile(user_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  @impl true
+  def handle_event("save", %{"user" => user_params}, socket) do
+    case socket.assigns.user
+         |> Bright.Repo.preload(:user_profile)
+         |> Accounts.update_user_with_user_profile(user_params) do
+      {:ok, _user} ->
+        {:noreply, socket |> put_flash(:info, "保存しました") |> push_navigate(to: ~p"/mypage")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    assign(socket, :form, to_form(changeset))
   end
 end
