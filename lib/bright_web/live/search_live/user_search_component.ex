1e50bacfd7f6107d3ddef751e6cebe785c26ef1c
thehaigo <sirannha@gmail.com>
2023-08-24 01:25:56 +0900

    remove unused

--- lib/bright_web/live/search_live/user_search_component.ex
+++ lib/bright_web/live/search_live/user_search_component.ex
@@ -1,7 +1,7 @@
 defmodule BrightWeb.SearchLive.UserSearchComponent do
   use BrightWeb, :live_component
 
-  alias Bright.{CareerFields, SkillPanels, Searches, Repo, Jobs}
+  alias Bright.{CareerFields, SkillPanels, Searches}
   alias Bright.SearchForm.{UserSearch, SkillSearch}
   alias Bright.UserJobProfiles.UserJobProfile
   alias BrightWeb.SearchLive.SearchResultComponent
