2c61c1347919d22d4c0abc49050fe33ae7d9586c
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-08-31 19:08:59 +0900

    order_by追加

--- lib/bright/recruitment_stock_users.ex
+++ lib/bright/recruitment_stock_users.ex
@@ -21,6 +21,7 @@ defmodule Bright.RecruitmentStockUsers do
     from(recruitment_stock_users in RecruitmentStockUser,
       join: user in assoc(recruitment_stock_users, :user),
       where: recruitment_stock_users.recruiter_id == ^recruiter_id,
+      order_by: [asc: recruitment_stock_users.inserted_at],
       select: user
     )
     |> Repo.all()
