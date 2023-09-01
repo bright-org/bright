eacbfaf0d4a65d0c17b3b796d7f997402f6917ef
thehaigo <sirannha@gmail.com>
2023-09-01 15:52:23 +0900

    カラム追加

---
+++ priv/repo/migrations/20230901062825_add_skill_panel_name_income_to_recruitment_stock_users.exs
@@ -0,0 +1,10 @@
+defmodule Bright.Repo.Migrations.AddSkillPanelNameIncomeToRecruitmentStockUsers do
+  use Ecto.Migration
+
+  def change do
+    alter table(:recruitment_stock_users) do
+      add :skill_panel, :string, default: "", null: false
+      add :desired_income, :integer, default: 0
+    end
+  end
+end
