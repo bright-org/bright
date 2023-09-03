58b78a0115a5d824298e82f1779cbad946e5a77a
koyo-miyamura <nexus9071@gmail.com>
2023-09-04 00:29:41 +0900

    Add test for UserConfirmEmailController

---
+++ test/bright_web/controllers/user_confirm_email_controller_test.exs
@@ -0,0 +1,82 @@
+defmodule BrightWeb.UserConfirmEmailControllerTest do
+  use BrightWeb.ConnCase, async: true
+
+  import Bright.Factory
+  alias Bright.Repo
+  alias Bright.Accounts
+  alias Bright.Accounts.User
+  alias Bright.Accounts.UserToken
+
+  import Ecto.Query, warn: false
+
+  setup [:register_and_log_in_user]
+
+  setup %{conn: conn, user: user} do
+    email = unique_user_email()
+
+    token =
+      extract_user_token(fn url ->
+        Accounts.deliver_user_update_email_instructions(%{user | email: email}, user.email, url)
+      end)
+
+    %{conn: conn, user: user, token: token, email: email}
+  end
+
+  describe "GET /users/confirm_email/:token" do
+    test "updates user email", %{
+      conn: conn,
+      user: user,
+      token: token,
+      email: email
+    } do
+      conn = get(conn, ~p"/users/confirm_email/#{token}")
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :info) == "メールアドレスの更新に成功しました"
+      assert redirected_to(conn) == ~p"/mypage"
+      assert Repo.get_by(User, email: email)
+      refute Repo.get_by(UserToken, user_id: user.id, context: "change:#{user.email}")
+    end
+
+    test "fails to update user email when token is invalid", %{
+      conn: conn,
+      user: user,
+      email: email
+    } do
+      conn = get(conn, ~p"/users/confirm_email/invalid")
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) == "リンクが無効であるか期限が切れています"
+      assert redirected_to(conn) == ~p"/mypage"
+      refute Repo.get_by(User, email: email)
+      assert Repo.get_by(UserToken, user_id: user.id, context: "change:#{user.email}")
+    end
+
+    test "fails to update user email when token is expired", %{
+      conn: conn,
+      user: user,
+      token: token,
+      email: email
+    } do
+      {1, nil} =
+        from(u in UserToken,
+          where:
+            u.user_id == ^user.id and
+              u.context ==
+                ^"change:#{user.email}"
+        )
+        |> Repo.update_all(
+          set: [
+            inserted_at:
+              NaiveDateTime.utc_now()
+              |> NaiveDateTime.add(-1 * 60 * 60 * 24)
+          ]
+        )
+
+      conn = get(conn, ~p"/users/confirm_email/#{token}")
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) == "リンクが無効であるか期限が切れています"
+      assert redirected_to(conn) == ~p"/mypage"
+      refute Repo.get_by(User, email: email)
+      assert Repo.get_by(UserToken, user_id: user.id, context: "change:#{user.email}")
+    end
+  end
+end
