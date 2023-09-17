b44d3216f2b857eebf13fee30ff2dea692963e82
koyo-miyamura <nexus9071@gmail.com>
2023-09-17 15:48:52 +0900

    スキーマの修正

--- priv/repo/migrations/20230917063937_create_user_sub_emails.exs
+++ priv/repo/migrations/20230917063937_create_user_sub_emails.exs
@@ -3,12 +3,13 @@ defmodule Bright.Repo.Migrations.CreateUserSubEmails do
 
   def change do
     create table(:user_sub_emails) do
-      add :email, :string
+      add :email, :citext, null: false
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
     end
 
     create index(:user_sub_emails, [:user_id])
+    create unique_index(:user_sub_emails, [:email])
   end
 end
