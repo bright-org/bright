95dc3753ae10e36d96cd367a1980804b63349f08
koyo-miyamura <nexus9071@gmail.com>
2023-10-02 04:27:39 +0900

    Add test for AuthSettingComponentTest

--- test/bright_web/live/user_settings_live/auth_setting_component_test.exs
+++ test/bright_web/live/user_settings_live/auth_setting_component_test.exs
@@ -4,8 +4,10 @@ defmodule BrightWeb.UserSettingsLive.AuthSettingComponentTest do
   alias Bright.Repo
   alias Bright.Accounts
   alias Bright.Accounts.UserToken
+  alias Bright.Accounts.UserSubEmail
   import Phoenix.LiveViewTest
   import Swoosh.TestAssertions
+  import Bright.Factory
 
   describe "メール・パスワード" do
     setup [:register_and_log_in_user]
@@ -18,9 +20,13 @@ defmodule BrightWeb.UserSettingsLive.AuthSettingComponentTest do
       assert lv |> has_element?("#email_form span", "メールアドレス")
       assert lv |> has_element?(~s{#email_form input[name="user[email]"][value="#{user.email}"]})
 
-      assert lv |> has_element?("span", "サブアドレス")
-      # TODO: fix test
-      # assert lv |> has_element?(~s{input[type="text"][disabled]})
+      assert lv |> has_element?("#sub_mail_section span", "サブアドレス")
+      assert lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+
+      refute lv
+             |> has_element?(~s{#sub_mail_section input[name="sub_mail_1"][disabled]})
+
+      refute lv |> has_element?("#delete_sub_email_button_1")
 
       assert lv |> has_element?("#password_form span", "現在のパスワード")
 
@@ -65,6 +71,35 @@ defmodule BrightWeb.UserSettingsLive.AuthSettingComponentTest do
       assert lv |> has_element?("#email_form .text-error", "変更されていません")
     end
 
+    test "validates email form when submitted email is not unique", %{
+      conn: conn
+    } do
+      other_user = insert(:user)
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      lv |> form("#email_form", user: %{email: other_user.email}) |> render_submit()
+
+      assert_no_email_sent()
+      assert lv |> has_element?("#email_form .text-error", "すでに使用されています")
+    end
+
+    test "validates email form when submitted email is not unique in user sub email", %{
+      conn: conn
+    } do
+      user_sub_email = insert(:user_sub_email)
+
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      lv |> form("#email_form", user: %{email: user_sub_email.email}) |> render_submit()
+
+      assert_no_email_sent()
+      assert lv |> has_element?("#email_form .text-error", "すでに使用されています")
+    end
+
     test "validates email form when email is invalid format", %{conn: conn} do
       {:ok, lv, _html} = live(conn, ~p"/mypage")
 
@@ -72,6 +107,140 @@ defmodule BrightWeb.UserSettingsLive.AuthSettingComponentTest do
 
       lv |> form("#email_form", user: %{email: "invalid_format"}) |> render_change()
       assert lv |> has_element?("#email_form .text-error", "無効なフォーマットです")
+    end
+
+    test "show sub email delete button and add form when user has 1 sub email", %{
+      conn: conn,
+      user: user
+    } do
+      user_sub_email = insert(:user_sub_email, user: user)
+
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      assert lv |> has_element?("#sub_mail_section span", "サブアドレス")
+
+      assert lv
+             |> has_element?(
+               ~s{#sub_mail_section input[name="sub_mail_1"][value="#{user_sub_email.email}"][disabled]}
+             )
+
+      assert lv |> has_element?("#delete_sub_email_button_1")
+
+      assert lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+    end
+
+    test "show sub email delete button and does not show add form when user has 3 sub email", %{
+      conn: conn,
+      user: user
+    } do
+      [user_sub_email1, user_sub_email2, user_sub_email3] =
+        insert_list(3, :user_sub_email, user: user)
+
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      assert lv |> has_element?("#sub_mail_section span", "サブアドレス")
+
+      assert lv
+             |> has_element?(
+               ~s{#sub_mail_section input[name="sub_mail_1"][value="#{user_sub_email1.email}"][disabled]}
+             )
+
+      assert lv |> has_element?("#delete_sub_email_button_1")
+
+      assert lv
+             |> has_element?(
+               ~s{#sub_mail_section input[name="sub_mail_2"][value="#{user_sub_email2.email}"][disabled]}
+             )
+
+      assert lv |> has_element?("#delete_sub_email_button_2")
+
+      assert lv
+             |> has_element?(
+               ~s{#sub_mail_section input[name="sub_mail_3"][value="#{user_sub_email3.email}"][disabled]}
+             )
+
+      assert lv |> has_element?("#delete_sub_email_button_3")
+
+      refute lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+    end
+
+    test "submits sub email form", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      new_email = unique_user_email()
+      lv |> form("#sub_email_form", user_sub_email: %{email: new_email}) |> render_submit()
+
+      assert_add_sub_email_mail_sent(new_email)
+      assert lv |> has_element?("#modal_flash", "サブメールアドレス追加確認メールを送信しました")
+
+      # NOTE: Reset input form after submit
+      assert lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+
+      lv |> refute_redirected(~p"/mypage")
+    end
+
+    test "clicks sub email delete button", %{conn: conn, user: user} do
+      insert_list(3, :user_sub_email, user: user)
+
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      assert lv |> has_element?("#delete_sub_email_button_1")
+      assert lv |> has_element?("#delete_sub_email_button_2")
+      assert lv |> has_element?("#delete_sub_email_button_3")
+      refute lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+
+      lv |> element("#delete_sub_email_button_2", "削除する") |> render_click()
+
+      assert lv |> has_element?("#modal_flash", "サブメールアドレスを削除しました")
+
+      assert Repo.aggregate(UserSubEmail, :count) == 2
+
+      assert lv |> has_element?("#delete_sub_email_button_1")
+      assert lv |> has_element?("#delete_sub_email_button_2")
+      refute lv |> has_element?("#delete_sub_email_button_3")
+      assert lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
+    end
+
+    test "validates sub email form when sub email is invalid format", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      lv |> form("#sub_email_form", user_sub_email: %{email: "invalid_format"}) |> render_change()
+      assert lv |> has_element?("#sub_email_form .text-error", "無効なフォーマットです")
+    end
+
+    test "validates email form when submitted sub email is not unique", %{conn: conn, user: user} do
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      lv |> form("#sub_email_form", user_sub_email: %{email: user.email}) |> render_submit()
+
+      assert_no_email_sent()
+      assert lv |> has_element?("#sub_email_form .text-error", "すでに使用されています")
+    end
+
+    test "validates email form when submitted sub email is not unique in sub email", %{conn: conn} do
+      user_sub_email = insert(:user_sub_email)
+      {:ok, lv, _html} = live(conn, ~p"/mypage")
+
+      lv |> element("a", "メール・パスワード") |> render_click()
+
+      lv
+      |> form("#sub_email_form", user_sub_email: %{email: user_sub_email.email})
+      |> render_submit()
+
+      assert_no_email_sent()
+      assert lv |> has_element?("#sub_email_form .text-error", "すでに使用されています")
     end
 
     test "submits password form", %{conn: conn, user: user, current_password: current_password} do
@@ -169,9 +338,8 @@ defmodule BrightWeb.UserSettingsLive.AuthSettingComponentTest do
       assert lv |> has_element?("#email_form span", "メールアドレス")
       assert lv |> has_element?(~s{#email_form input[name="user[email]"][value="#{user.email}"]})
 
-      assert lv |> has_element?("span", "サブアドレス")
-      # TODO: fix test
-      # assert lv |> has_element?(~s{input[type="text"][disabled]})
+      assert lv |> has_element?("#sub_mail_section span", "サブアドレス")
+      assert lv |> has_element?(~s{#sub_email_form input[name="user_sub_email[email]"][value=""]})
 
       # NOTE: SNS ID ログインのユーザーはパスワードを設定できないので表示しない
       refute lv |> has_element?("#password_form span", "現在のパスワード")
