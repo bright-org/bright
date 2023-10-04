7c66a7e732247608f88cab08609d1713892d322d
koyo-miyamura <nexus9071@gmail.com>
2023-10-04 11:31:41 +0900

    Add oauth_controller_test for GitHub

--- test/bright_web/controllers/oauth_controller_test.exs
+++ test/bright_web/controllers/oauth_controller_test.exs
@@ -9,6 +9,7 @@ defmodule BrightWeb.OAuthControllerTest do
 
   @name "koyo"
   @email "dummy@example.com"
+  @github_nickname "koyo-miyamura"
 
   describe "GET /auth/:provider" do
     test "redirects when provider is google", %{conn: conn} do
@@ -24,6 +25,25 @@ defmodule BrightWeb.OAuthControllerTest do
                "redirect_uri" => "http://www.example.com/auth/google/callback",
                "response_type" => "code",
                "scope" => "email profile",
+               "state" => state
+             } = URI.decode_query(query)
+
+      assert is_binary(state)
+    end
+
+    test "redirects when provider is github", %{conn: conn} do
+      conn = get(conn, ~p"/auth/github")
+
+      assert %URI{
+               path: "/login/oauth/authorize",
+               query: query
+             } = redirected_to(conn) |> URI.parse()
+
+      assert %{
+               "client_id" => "dummy_client_id",
+               "redirect_uri" => "http://www.example.com/auth/github/callback",
+               "response_type" => "code",
+               "scope" => "",
                "state" => state
              } = URI.decode_query(query)
 
@@ -45,6 +65,22 @@ defmodule BrightWeb.OAuthControllerTest do
       |> Map.get("state")
 
     %{conn: conn |> recycle(), state: state, provider: :google}
+  end
+
+  defp setup_for_github_auth(%{conn: conn}) do
+    # NOTE: state パラメータ付きでリクエストしないと CSRF エラーになるので事前に生成しておく
+    conn =
+      conn
+      |> get(~p"/auth/github")
+
+    state =
+      redirected_to(conn)
+      |> URI.parse()
+      |> Map.get(:query)
+      |> URI.decode_query()
+      |> Map.get("state")
+
+    %{conn: conn |> recycle(), state: state, provider: :github}
   end
 
   # NOTE: Bright.Ueberauth.Strategy.Test を前提としたテスト
@@ -185,7 +221,120 @@ defmodule BrightWeb.OAuthControllerTest do
     end
   end
 
-  describe "GET /auth/:provider/callback when user logs in" do
+  describe "GET /auth/:provider/callback when provider is github" do
+    setup [:setup_for_github_auth]
+
+    test "redirects and logs when OAuth is failure", %{
+      conn: conn,
+      state: state,
+      provider: provider
+    } do
+      ueberauth_failure = %Ueberauth.Failure{
+        errors: %Ueberauth.Failure.Error{
+          message: "error message",
+          message_key: "error message_key"
+        },
+        provider: provider,
+        strategy: Ueberauth.Strategy.Github
+      }
+
+      log =
+        capture_log([level: :warn], fn ->
+          conn =
+            conn
+            |> assign(:ueberauth_failure, ueberauth_failure)
+            |> get(~p"/auth/github/callback", %{"state" => state})
+
+          assert redirected_to(conn) == ~p"/users/register"
+          assert Phoenix.Flash.get(conn.assigns.flash, :error) == "認証に失敗しました"
+        end)
+
+      assert log =~ inspect(ueberauth_failure)
+    end
+
+    test "generates social identifier token and redirects when OAuth is success and no user by provider and identifier exists",
+         %{
+           conn: conn,
+           state: state,
+           provider: provider
+         } do
+      identifier = 1
+
+      ueberauth_auth = %Ueberauth.Auth{
+        info: %Ueberauth.Auth.Info{name: @name, email: @email, nickname: @github_nickname},
+        provider: provider,
+        uid: identifier
+      }
+
+      conn =
+        conn
+        |> assign(:ueberauth_auth, ueberauth_auth)
+        |> get(~p"/auth/github/callback", %{"state" => state})
+
+      # 具体的なリダイレクト先はハッシュ化された token によって決まるので部分一致で判定する
+      assert redirected_to(conn) =~ "/users/register_social_account/"
+
+      assert %SocialIdentifierToken{name: @name, email: @email, display_name: @github_nickname} =
+               Repo.get_by(SocialIdentifierToken,
+                 provider: provider,
+                 identifier: to_string(identifier)
+               )
+    end
+
+    test "redirects log in page in when OAuth is success and user by provider and identifier is already registered and but not confirmed",
+         %{
+           conn: conn,
+           state: state,
+           provider: provider
+         } do
+      identifier = 1
+      user = insert(:user_not_confirmed)
+      insert(:user_social_auth_for_github, user: user, identifier: to_string(identifier))
+
+      ueberauth_auth = %Ueberauth.Auth{
+        info: %Ueberauth.Auth.Info{name: @name, nickname: @github_nickname},
+        provider: provider,
+        uid: identifier
+      }
+
+      conn =
+        conn
+        |> assign(:ueberauth_auth, ueberauth_auth)
+        |> get(~p"/auth/github/callback", %{"state" => state})
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :error) ==
+               "メールアドレス未確認ユーザーです。メールを確認して確認済みにしてください。"
+
+      assert redirected_to(conn) == ~p"/users/log_in"
+    end
+
+    test "logs in when OAuth is success and user by provider and identifier is already registered and confirmed",
+         %{
+           conn: conn,
+           state: state,
+           provider: provider
+         } do
+      identifier = 1
+      insert(:user_social_auth_for_github, identifier: to_string(identifier))
+
+      ueberauth_auth = %Ueberauth.Auth{
+        info: %Ueberauth.Auth.Info{name: @name, nickname: @github_nickname},
+        provider: provider,
+        uid: identifier
+      }
+
+      conn =
+        conn
+        |> assign(:ueberauth_auth, ueberauth_auth)
+        |> get(~p"/auth/github/callback", %{"state" => state})
+
+      assert get_session(conn, :user_token)
+      assert conn.resp_cookies["_bright_web_user"]
+      assert redirected_to(conn) == ~p"/onboardings/welcome"
+    end
+  end
+
+  describe "GET /auth/:provider/callback when provider is google and user logs in" do
     setup [:setup_for_google_auth, :register_and_log_in_user]
 
     test "links social accounts", %{conn: conn, user: user, state: state, provider: provider} do
@@ -256,6 +405,40 @@ defmodule BrightWeb.OAuthControllerTest do
       assert Phoenix.Flash.get(conn.assigns.flash, :error) == "連携に失敗しました"
       assert redirected_to(conn) == ~p"/mypage"
       assert Repo.aggregate(UserSocialAuth, :count) == 1
+    end
+  end
+
+  describe "GET /auth/:provider/callback when provider is github and user logs in" do
+    setup [:setup_for_github_auth, :register_and_log_in_user]
+
+    test "links social accounts", %{
+      conn: conn,
+      user: %{id: user_id},
+      state: state,
+      provider: provider
+    } do
+      identifier = 1
+
+      ueberauth_auth = %Ueberauth.Auth{
+        info: %Ueberauth.Auth.Info{name: @name, email: @email, nickname: @github_nickname},
+        provider: provider,
+        uid: identifier
+      }
+
+      conn =
+        conn
+        |> assign(:ueberauth_auth, ueberauth_auth)
+        |> get(~p"/auth/github/callback", %{"state" => state})
+
+      assert Phoenix.Flash.get(conn.assigns.flash, :info) == "連携しました"
+      assert redirected_to(conn) == ~p"/mypage"
+
+      identifier = to_string(identifier)
+
+      assert %UserSocialAuth{
+               display_name: @github_nickname,
+               identifier: ^identifier
+             } = Repo.get_by(UserSocialAuth, user_id: user_id, provider: provider)
     end
   end
 
