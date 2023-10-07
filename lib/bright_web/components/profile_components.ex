a7a46fa589a75b13842744b7fec738dce420a512
thehaigo <sirannha@gmail.com>
2023-10-08 01:02:53 +0900

    test fix

--- lib/bright_web/components/profile_components.ex
+++ lib/bright_web/components/profile_components.ex
@@ -77,7 +77,7 @@ defmodule BrightWeb.ProfileComponents do
            <.profile_button :if={@display_recruitment_coordination}>採用の調整</.profile_button>
           </div>
         </div>
-        <div class="flex flex-col lg:flex-row justify-between pt-3 border-brightGray-100 border-t">
+        <div class="flex flex-col lg:flex-row justify-between pt-3 border-brightGray-100 border-t w-64 lg:w-full">
           <div class="text-base lg:text-xl mb-4 max-w-[600px] break-all"><%= @title %></div>
           <.sns :if={@display_sns} twitter_url={@twitter_url} github_url={@github_url} facebook_url={@facebook_url} />
         </div>
