8dfaa90c91101b0dd8c75eee2f79ffb3d84d0888
thehaigo <sirannha@gmail.com>
2023-10-11 15:54:00 +0900

    ユーザーのハンドル名に全角文字許容、文字制限を30に設定

--- lib/bright/accounts/user.ex
+++ lib/bright/accounts/user.ex
@@ -101,12 +101,13 @@ defmodule Bright.Accounts.User do
   defp validate_name(changeset, opts) do
     changeset
     |> validate_required([:name])
-    |> validate_length(:name, max: 255)
-    |> validate_format(
-      :name,
-      ~r/^([0-9a-z-_.])*$/,
-      message: "only lower-case alphanumeric character and -_. is available"
-    )
+    |> validate_length(:name, max: 30)
+    # 全角を許容、運用上問題があった場合のためにコメントアウトに留める
+    #    |> validate_format(
+    #      :name,
+    #      ~r/^([0-9a-z-_.])*$/,
+    #      message: "only lower-case alphanumeric character and -_. is available"
+    #    )
     |> maybe_validate_unique_name(opts)
   end
 
