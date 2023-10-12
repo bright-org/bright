f699249c0ece69039fa6073412c4d9a87e8c21f4
thehaigo <sirannha@gmail.com>
2023-10-12 16:57:38 +0900

    ogp setting add meta twitter summary_large_iamge

--- lib/bright_web/components/layout_components.ex
+++ lib/bright_web/components/layout_components.ex
@@ -37,6 +37,7 @@ defmodule BrightWeb.LayoutComponents do
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content={@csrf_token} />
+        <meta name="twitter:card" content="summary_large_image">
         <meta property="og:title" content="Bright｜過去と今、未来のスキルから、あなたの輝きを見える化します">
         <meta property="og:description" content={"ITで世の中に価値をもたらすエンジニアやインフラ、デザイナー、マーケッターから、気になるスキルを選び、あなたがどのような\"輝き\"を放つかを体験してください。"}>
         <meta property="og:image" content="https://bright-fun.org/images/ogp_a.png">
