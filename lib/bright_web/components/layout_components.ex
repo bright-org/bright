2023865b898b261bf53b396f89740bfb4c84cde0
thehaigo <sirannha@gmail.com>
2023-10-14 00:34:57 +0900

    サイドバーにリンク追加、見切れたときにスクロールできるように変更

--- lib/bright_web/components/layout_components.ex
+++ lib/bright_web/components/layout_components.ex
@@ -129,7 +129,7 @@ defmodule BrightWeb.LayoutComponents do
         <span class="absolute bg-brightGray-300 block cursor-pointer h-[3px] left-1 top-1.5 w-8 before:bg-brightGray-300 before:block before:content-[''] before:cursor-pointer before:h-[3px] before:absolute before:top-3 before:w-8 after:bg-brightGray-300 after:block after:content-[''] after:cursor-pointer after:h-[3px] after:absolute after:top-6 after:w-8"></span>
       </label>
       <label id="sp_navi_close" for="sp_navi_input" class="cursor-pointer hidden h-full fixed right-0 top-0 w-full z-20 -ml-2"></label>
-      <div class="fixed bg-brightGray-900 pt-3 min-h-screen h-full hidden flex-col w-full z-40 lg:flex lg:static lg:w-[200px] peer-checked:flex">
+      <div class="fixed bg-brightGray-900 pt-3 h-full hidden flex-col w-full z-40 lg:flex lg:static lg:w-[200px] peer-checked:flex overflow-y-scroll">
         <.link href="/mypage"><img src="/images/common/logo.svg" width="163px" class="ml-2 lg:ml-4 mt-12 lg:mt-0" /></.link>
         <ul class="grid lg:pt-2">
           <%= for {title, path, regex} <- links() do %>
@@ -154,6 +154,7 @@ defmodule BrightWeb.LayoutComponents do
 
   def links() do
     [
+      {"保有スキル／所属", "/mypage", nil},
       {"スキルを選ぶ", "/more_skills?open=want_todo_panel", nil},
       {"成長を見る・比較する", "/graphs", nil},
       {"スキルを入力", "/panels", nil},
