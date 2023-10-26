a55250d6306a44b876017e366142fde56ebbde20
koyo-miyamura <nexus9071@gmail.com>
2023-10-26 11:12:22 +0900

    confirmed_at の追加とインデックスの追加

---
+++ priv/repo/migrations/20231022151552_notification_confirmed_at.exs
@@ -0,0 +1,18 @@
+defmodule Bright.Repo.Migrations.NotificationConfirmedAt do
+  use Ecto.Migration
+
+  def change do
+    alter table(:notification_operations) do
+      add :confirmed_at, :naive_datetime
+    end
+
+    alter table(:notification_communities) do
+      add :confirmed_at, :naive_datetime
+    end
+
+    create index(:notification_operations, [:confirmed_at])
+    create index(:notification_operations, [:inserted_at, :id])
+    create index(:notification_communities, [:confirmed_at])
+    create index(:notification_communities, [:inserted_at, :id])
+  end
+end
