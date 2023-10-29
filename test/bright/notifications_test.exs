7267688d80b460661fa655e085dd356bcb0e2976
koyo-miyamura <nexus9071@gmail.com>
2023-10-29 21:33:41 +0900

    list_notification_by_type/3 のテストがなかったので追加

--- test/bright/notifications_test.exs
+++ test/bright/notifications_test.exs
@@ -20,6 +20,49 @@ defmodule Bright.NotificationsTest do
     end
   end
 
+  describe "list_notification_by_type/3" do
+    setup do
+      from_user = insert(:user)
+
+      {:ok,
+       from_user: from_user,
+       notification_operations:
+         insert_list(3, :notification_operation, from_user: from_user)
+         |> Enum.sort_by(& &1.id)
+         |> Enum.reverse(),
+       notification_communities:
+         insert_list(3, :notification_community, from_user: from_user)
+         |> Enum.sort_by(& &1.id)
+         |> Enum.reverse()}
+    end
+
+    test "for type operation", %{notification_operations: notification_operations} do
+      assert %{
+               entries: entries,
+               page_number: 1,
+               page_size: 2,
+               total_entries: 3,
+               total_pages: 2
+             } = Notifications.list_notification_by_type("", "operation", page: 1, page_size: 2)
+
+      assert entries |> Enum.map(& &1.id) ==
+               notification_operations |> Enum.take(2) |> Enum.map(& &1.id)
+    end
+
+    test "for type community", %{notification_communities: notification_communities} do
+      assert %{
+               entries: entries,
+               page_number: 1,
+               page_size: 2,
+               total_entries: 3,
+               total_pages: 2
+             } = Notifications.list_notification_by_type("", "community", page: 1, page_size: 2)
+
+      assert entries |> Enum.map(& &1.id) ==
+               notification_communities |> Enum.take(2) |> Enum.map(& &1.id)
+    end
+  end
+
   describe "confirm_notification!/1" do
     test "for type operation" do
       notification = insert(:notification_operation, confirmed_at: nil)
