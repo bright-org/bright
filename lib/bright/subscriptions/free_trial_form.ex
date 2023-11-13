f493749fde3a4215f8531d6496963217626cd1ad
thehaigo <sirannha@gmail.com>
2023-11-13 22:27:30 +0900

    フリートライアルモーダル作成

---
+++ lib/bright/subscriptions/free_trial_form.ex
@@ -0,0 +1,30 @@
+defmodule Bright.Subscriptions.FreeTrialForm do
+  @moduledoc """
+  フリートライアル申込時のフォームモデル
+  """
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  embedded_schema do
+    field :user_id, :string
+    field :plan_name, :string
+    field :company_name, :string
+    field :phone_number, :string
+    field :email, :string
+    field :pic_name, :string
+  end
+
+  @doc false
+  def changeset(free_trial, attrs) do
+    free_trial
+    |> cast(attrs, [
+      :user_id,
+      :plan_name,
+      :company_name,
+      :phone_number,
+      :email,
+      :pic_name
+    ])
+  end
+end
