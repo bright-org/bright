282bd6fa53ebef432a1b0850119f49195730093f
takuto <takuto4devops@gmail.com>
2023-12-08 06:59:51 +0900

    #1229 不要になったimport削除

--- lib/bright_web/live/team_support_live/team_support_live_componet.ex
+++ lib/bright_web/live/team_support_live/team_support_live_componet.ex
@@ -11,7 +11,6 @@ defmodule BrightWeb.TeamSupportLiveComponent do
   import BrightWeb.ProfileComponents
   import BrightWeb.TeamComponents
   import BrightWeb.TabComponents
-  import BrightWeb.CardLive.CardListComponents
 
   alias Bright.UserProfiles
   alias alias Bright.Teams
