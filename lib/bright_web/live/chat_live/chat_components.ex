b4f76b08940561f104b973deabc175857e4cfb5d
thehaigo <sirannha@gmail.com>
2024-02-03 01:04:11 +0900

    障害対応に合わせてリファクタ

---
+++ lib/bright_web/live/chat_live/chat_components.ex
@@ -0,0 +1,127 @@
+defmodule BrightWeb.ChatLive.ChatComponents do
+  use BrightWeb, :component
+
+  alias Bright.Recruits.Interview
+  alias Bright.UserProfiles
+  import BrightWeb.CardLive.CardListComponents, only: [elapsed_time: 1]
+
+  attr :chat, :any, required: true
+  attr :selected_chat, :any, required: true
+  attr :user_id, :string, required: true
+
+  def chat_list(assigns) do
+    ~H"""
+    <.link
+      class={"flex py-4 px-4 justify-center items-center border-b-2 cursor-pointer #{if @selected_chat != nil && @selected_chat.id == @chat.id, do: "border-l-4 border-l-blue-400"}"}
+      patch={~p"/recruits/chats/#{@chat.id}"}
+    >
+      <div class="mr-2">
+        <.switch_user_icon chat={@chat} show_name={false} user_id={@user_id}/>
+      </div>
+      <div class="w-full flex justify-between p-1">
+        <div class="flex-1 mr-2 lg:truncate lg:text-xl">
+          <span>
+            <%= if @chat.interview.skill_panel_name == nil ,
+              do: "スキルパネルデータなし",
+              else: @chat.interview.skill_panel_name
+            %>
+          </span>
+          <br />
+          <span class="text-brightGray-300">
+            <%= NaiveDateTime.to_date(@chat.interview.inserted_at) %>
+            希望年収:<%= @chat.interview.desired_income %>
+          </span>
+        </div>
+        <.elapsed_time inserted_at={@chat.updated_at} />
+      </div>
+    </.link>
+    """
+  end
+
+  attr :current_user, :any, required: true
+  attr :chat, :any, required: true
+  attr :message, :any, required: true
+  attr :sender_icon_path, :string, required: true
+
+  def message(assigns) do
+    ~H"""
+    <%= if @current_user.id == @message.sender_user_id do %>
+      <div class="flex justify-end">
+        <div class="flex flex-col mb-4">
+          <div class="flex">
+            <div class="text-xl mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+              <%= nl_to_br(@message.text) %>
+            </div>
+            <div>
+              <.user_icon path={@sender_icon_path} />
+              <span><%= @current_user.name %></span>
+            </div>
+          </div>
+          <p class="mt-1"><.elapsed_time inserted_at={@message.inserted_at} /></p>
+        </div>
+      </div>
+    <% else %>
+      <div class="flex justify-start mb-4">
+        <.switch_user_icon
+          chat={@chat}
+          user_id={@current_user.id}
+          anon={@chat.interview.recruiter_user_id == @current_user.id}
+        />
+        <div class="text-xl ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+          <%= nl_to_br(@message.text) %>
+        </div>
+      </div>
+      <p class="-ml-4"><.elapsed_time inserted_at={@message.inserted_at} /></p>
+    <% end %>
+    """
+  end
+
+  attr :chat, :any, required: true
+  attr :show_name, :boolean, default: true
+  attr :user_id, :string, required: true
+  attr :anon, :boolean, default: true
+
+  def switch_user_icon(assigns) do
+    assigns = set_user(assigns)
+
+    ~H"""
+      <%= if @anon and Interview.anon?(@chat.interview) do %>
+        <.user_icon path={nil} />
+      <% else %>
+        <div class="flex flex-col justify-end">
+          <.user_icon path={@user.icon}/>
+          <p :if={@show_name} class="w-24 break-words"><%= @user.name %></p>
+        </div>
+      <% end %>
+    """
+  end
+
+  def user_icon(assigns) do
+    ~H"""
+      <img
+      src={UserProfiles.icon_url(@path)}
+      class="object-cover h-10 w-10 rounded-full mt-4"
+      alt=""
+    />
+    """
+  end
+
+  defp set_user(assigns) do
+    user =
+      if assigns.chat.owner_user_id == assigns.user_id do
+        %{
+          name: assigns.chat.interview.candidates_user_name,
+          icon: assigns.chat.interview.candidates_user_icon
+        }
+      else
+        %{
+          name: assigns.chat.interview.recruiter_user_name,
+          icon: assigns.chat.interview.recruiter_user_icon
+        }
+      end
+
+    Map.put(assigns, :user, user)
+  end
+
+  defp nl_to_br(str), do: str |> String.replace(~r/\n/, "<br />") |> Phoenix.HTML.raw()
+end
--- lib/bright_web/live/chat_live/index.ex
+++ lib/bright_web/live/chat_live/index.ex
@@ -4,10 +4,8 @@ defmodule BrightWeb.ChatLive.Index do
   alias Bright.Chats
   alias Bright.Accounts
   alias Bright.Recruits
-  alias Bright.Recruits.Interview
-  alias Bright.UserProfiles
-  alias BrightWeb.CardLive.CardListComponents
 
+  import BrightWeb.ChatLive.ChatComponents
   import BrightWeb.BrightModalComponents, only: [bright_modal: 1]
 
   @impl true
@@ -24,164 +22,64 @@ defmodule BrightWeb.ChatLive.Index do
           </p>
         <% else %>
           <%= for chat <- @chats do %>
-            <.link
-              class={"flex py-4 px-4 justify-center items-center border-b-2 cursor-pointer #{if @chat != nil && chat.id == @chat.id, do: "border-l-4 border-l-blue-400"}"}
-              patch={~p"/recruits/chats/#{chat.id}"}
-            >
-              <% path = if Interview.anon?(chat.interview), do: nil, else: chat.interview.candidates_user_icon %>
-              <img
-                src={UserProfiles.icon_url(path)}
-                class="object-cover h-10 w-10 rounded-full mr-2"
-                alt=""
-              />
-              <div class="w-full flex justify-between p-1">
-                <div class="flex-1 mr-2 lg:truncate lg:text-xl">
-                <span><%= if chat.interview.skill_panel_name == nil , do: "スキルパネルデータなし", else: chat.interview.skill_panel_name %></span>
-                <br />
-                <span class="text-brightGray-300">
-                <%= NaiveDateTime.to_date(chat.interview.inserted_at) %>
-                希望年収:<%= chat.interview.desired_income %>
-                </span>
-                </div>
-                <div>
-                  <CardListComponents.elapsed_time inserted_at={chat.updated_at} />
-                </div>
-              </div>
-            </.link>
+            <.chat_list chat={chat} selected_chat={@chat} user_id={@current_user.id}/>
           <% end %>
         <% end %>
       </div>
-      <!-- message -->
+
       <div class="w-full px-5 flex flex-col justify-between overflow-y-auto">
-        <div
-          class="flex flex-col mt-5"
-          :if={@chat}
-        >
-          <p class="lg:ml-12 text-xl mb-2">
-          ※メールアドレスや電話番号等の個人情報は送らないでください
-          </p>
-          <p class="lg:ml-12 text-xl mb-8">
-          ※面談日時およびその重複は管理対象外ですので、別途管理を行ってください
-          </p>
+        <div :if={@chat} class="flex flex-col mt-5" >
+          <p class="lg:ml-12 text-xl mb-2">※メールアドレスや電話番号等の個人情報は送らないでください</p>
+          <p class="lg:ml-12 text-xl mb-8">※面談日時およびその重複は管理対象外ですので、別途管理を行ってください</p>
           <%= if Enum.count(@messages) == 0 do %>
-          <div
-            class="lg:ml-12 text-xl font-bold"
-            :if={@current_user.id == @chat.owner_user_id && Accounts.hr_enabled?(@current_user.id)}
-          >
-            本チャットで面談候補者と面談の調整を行い、「面談確定の確認」ボタンを押してください
-          </div>
+            <div :if={@current_user.id == @chat.owner_user_id && Accounts.hr_enabled?(@current_user.id)} class="lg:ml-12 text-xl font-bold">
+              本チャットで面談候補者と面談の調整を行い、「面談確定の確認」ボタンを押してください
+            </div>
           <% else %>
             <%= for message <- @messages do %>
-              <%= if @current_user.id == message.sender_user_id do %>
-              <div class="flex justify-end mb-4">
-                <div class="text-xl mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                  <%= nl_to_br(message.text) %>
-                </div>
-                <div>
-                  <img
-                    src={@sender_icon_path}
-                    class="object-cover h-10 w-10 rounded-full mt-4"
-                    alt=""
-                  />
-                  <span><%= @current_user.name %></span>
-                </div>
-              </div>
-
-              <% else %>
-              <div class="flex justify-start mb-4">
-                <%= if @chat.owner_user_id == @current_user.id do %>
-                  <%= if Interview.anon?(@chat.interview) do %>
-                    <img
-                      src={UserProfiles.icon_url(nil)}
-                      class="object-cover h-10 w-10 rounded-full mt-4"
-                      alt=""
-                    />
-                  <% else %>
-                    <div class="flex flex-col justify-end">
-                      <img
-                        src={UserProfiles.icon_url(@chat.interview.candidates_user_icon)}
-                        class="object-cover h-10 w-10 rounded-full mt-4"
-                        alt=""
-                      />
-                      <p class="w-24 break-words"><%= @chat.interview.candidates_user_name %></p>
-                    </div>
-                  <% end %>
-
-                <% else %>
-                  <div class="flex flex-col justify-end">
-                    <img
-                      src={UserProfiles.icon_url(@chat.interview.recruiter_user_icon)}
-                      class="object-cover h-10 w-10 rounded-full mt-4"
-                      alt=""
-                    />
-                    <p class="w-24 break-words"><%= @chat.interview.recruiter_user_name %></p>
-                  </div>
-                <% end %>
-
-                <div class="text-xl ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                <%= nl_to_br(message.text) %>
-                </div>
-              </div>
-              <% end %>
+              <.message
+                chat={@chat}
+                message={message}
+                current_user={@current_user}
+                sender_icon_path={@sender_icon_path}
+              />
             <% end %>
           <% end %>
         </div>
-        <div
-          class="py-5 sticky bottom-0 bg-white mb-12 lg:mb-0"
-          :if={@chat}
-        >
+        <div :if={@chat} class="py-5 sticky bottom-0 bg-white mb-12 lg:mb-0">
           <form phx-submit="send">
             <div class="flex pb-2">
               <div class="w-[50px] flex justify-center flex-col items-center">
-                <img
-                  class="inline-block h-10 w-10 rounded-full"
-                  src={@sender_icon_path}
-                />
+                <.user_icon path={@sender_icon_path} />
               </div>
               <div class="w-full">
-                  <textarea
-                    class="w-full min-h-1 outline-none p-2"
-                    placeholder="メッセージを入力"
-                    name="message"
-                    value={@message}
-                  />
+                <textarea
+                  class="w-full min-h-1 outline-none p-2"
+                  placeholder="メッセージを入力"
+                  name="message"
+                  value={@message}
+                />
               </div>
             </div>
 
-            <!-- モーダル内フッター -->
-            <hr class="pb-1 border-brightGray-100">
+            <hr class="pb-1 border-brightGray-100" />
             <div class="flex justify-end gap-x-4 pt-2 pb-2 relative w-full">
               <button class="mr-auto">
-                <span class="material-icons-outlined !text-4xl opacity-50">
-                  add_photo_alternate
-                </span>
-                <span class="material-symbols-outlined !text-4xl opacity-50">
-                  add_box
-                </span>
+                <span class="material-icons-outlined !text-4xl opacity-50">add_photo_alternate</span>
+                <span class="material-symbols-outlined !text-4xl opacity-50">add_box</span>
               </button>
               <.link navigate={~p"/recruits/chats"}>
-                <button
-                  type="button"
-                  class="text-sm font-bold ml-auto px-2 py-[10px] rounded border bg-white w-24 lg:hidden"
-                >
+                <button type="button" class="text-sm font-bold ml-auto px-2 py-[10px] rounded border bg-white w-24 lg:hidden">
                   一覧に戻る
                 </button>
               </.link>
               <div class="flex">
-                <.link
-                  phx-click="close_chat"
-                  data-confirm="このチャットを閉じますか？"
-                >
-                  <button
-                    type="button"
-                    class="text-sm font-bold mr-2 px-2 py-3 rounded border bg-white w-36 lg:w-56"
-                  >
+                <.link phx-click="close_chat" data-confirm="このチャットを閉じますか？">
+                  <button type="button" class="text-sm font-bold mr-2 px-2 py-3 rounded border bg-white w-36 lg:w-56">
                     チャットを閉じる
                   </button>
                 </.link>
-                <div
-                  :if={@chat.owner_user_id == @current_user.id and @chat.interview.status != :cancel_interview and Accounts.hr_enabled?(@current_user.id)}
-                >
+                <div :if={@chat.owner_user_id == @current_user.id and Accounts.hr_enabled?(@current_user.id)}>
                   <%= if @chat.interview.status == :consume_interview do %>
                     <button
                       class="text-sm font-bold ml-auto px-2 py-3 rounded border bg-base text-white w-56"
@@ -209,46 +107,45 @@ defmodule BrightWeb.ChatLive.Index do
                 </button>
               </div>
             </div>
-            <!-- end message -->
           </form>
         </div>
       </div>
 
       <.bright_modal :if={@chat && @open_confirm_interview} id="interview-confirm-modal" show on_cancel={JS.navigate(~p"/recruits/chats/#{@chat.id}")}>
-      <.live_component
-        module={BrightWeb.RecruitInterviewLive.ConfirmComponent}
-        id="interview_confirm_modal"
-        title={@page_title}
-        action={@live_action}
-        interview_id={@chat.relation_id}
-        current_user={@current_user}
-        patch={~p"/recruits/chats/#{@chat.id}"}
-      />
-    </.bright_modal>
+        <.live_component
+          module={BrightWeb.RecruitInterviewLive.ConfirmComponent}
+          id="interview_confirm_modal"
+          title={@page_title}
+          action={@live_action}
+          interview_id={@chat.relation_id}
+          current_user={@current_user}
+          patch={~p"/recruits/chats/#{@chat.id}"}
+        />
+      </.bright_modal>
 
-    <.bright_modal :if={@chat && @open_cancel_interview}  id="interview-cancel-modal" show on_cancel={JS.navigate(~p"/recruits/chats/#{@chat.id}")}>
-      <.live_component
-        module={BrightWeb.RecruitInterviewLive.CancelComponent}
-        id="interview_cancel_modal"
-        title={@page_title}
-        action={@live_action}
-        interview_id={@chat.relation_id}
-        current_user={@current_user}
-        patch={~p"/recruits/chats"}
-        return_to={~p"/recruits/chats/#{@chat.id}"}
-      />
-    </.bright_modal>
+      <.bright_modal :if={@chat && @open_cancel_interview}  id="interview-cancel-modal" show on_cancel={JS.navigate(~p"/recruits/chats/#{@chat.id}")}>
+        <.live_component
+          module={BrightWeb.RecruitInterviewLive.CancelComponent}
+          id="interview_cancel_modal"
+          title={@page_title}
+          action={@live_action}
+          interview_id={@chat.relation_id}
+          current_user={@current_user}
+          patch={~p"/recruits/chats"}
+          return_to={~p"/recruits/chats/#{@chat.id}"}
+        />
+      </.bright_modal>
 
-    <.bright_modal :if={@chat && @open_create_coordination}  id="coordination-create-modal" show on_cancel={JS.navigate(~p"/recruits/chats/#{@chat.id}")}>
-      <.live_component
-        id="coordination_modal"
-        module={BrightWeb.RecruitCoordinationLive.CreateComponent}
-        current_user={@current_user}
-        interview_id={@chat.relation_id}
-        :if={@current_user}
-        patch={~p"/recruits/chats/#{@chat.id}"}
-      />
-    </.bright_modal>
+      <.bright_modal :if={@chat && @open_create_coordination}  id="coordination-create-modal" show on_cancel={JS.navigate(~p"/recruits/chats/#{@chat.id}")}>
+        <.live_component
+          id="coordination_modal"
+          module={BrightWeb.RecruitCoordinationLive.CreateComponent}
+          current_user={@current_user}
+          interview_id={@chat.relation_id}
+          :if={@current_user}
+          patch={~p"/recruits/chats/#{@chat.id}"}
+        />
+      </.bright_modal>
     </div>
     """
   end
@@ -259,7 +156,7 @@ defmodule BrightWeb.ChatLive.Index do
     |> assign(:open_confirm_interview, false)
     |> assign(:open_cancel_interview, false)
     |> assign(:open_create_coordination, false)
-    |> assign(:sender_icon_path, UserProfiles.icon_url(user.user_profile.icon_file_path))
+    |> assign(:sender_icon_path, user.user_profile.icon_file_path)
     |> then(&{:ok, &1})
   end
 
@@ -345,8 +242,6 @@ defmodule BrightWeb.ChatLive.Index do
     |> assign(:messages, messages ++ [message])
     |> then(&{:noreply, &1})
   end
-
-  defp nl_to_br(str), do: str |> String.replace(~r/\n/, "<br />") |> Phoenix.HTML.raw()
 
   defp send_new_message_notification_mails(chat, sender) do
     chat.users
