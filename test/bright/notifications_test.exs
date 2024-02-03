ad0b84a6d7e53d6e818a8ee3517dcfc6551a39d4
koyo-miyamura <nexus9071@gmail.com>
2024-02-03 16:33:39 +0900

    Add test for get_notification/2

--- test/bright/notifications_test.exs
+++ test/bright/notifications_test.exs
@@ -31,6 +31,18 @@ defmodule Bright.NotificationsTest do
     end
   end
 
+  describe "get_notification/2" do
+    test "for type operation" do
+      notification = insert(:notification_operation)
+
+      assert Notifications.get_notification("operation", notification.id)
+
+      other_notification = insert(:notification_community)
+
+      refute Notifications.get_notification("operation", other_notification.id)
+    end
+  end
+
   describe "get_notification!/2" do
     test "for type operation" do
       notification = insert(:notification_operation)
