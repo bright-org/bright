85e60037097537281f6b1dc2b029a01f6870f6a6
thehaigo <sirannha@gmail.com>
2024-03-26 10:42:47 +0900

    検索結果にプロフィールコンポーネントを追加

--- lib/bright_web/live/search_live/search_result_component.ex
+++ lib/bright_web/live/search_live/search_result_component.ex
@@ -2,12 +2,14 @@ defmodule BrightWeb.SearchLive.SearchResultComponent do
   use BrightWeb, :live_component
 
   alias Bright.SkillScores
+  alias Bright.UserProfiles
 
   import BrightWeb.TabComponents
   import BrightWeb.ChartComponents, only: [skill_gem: 1]
   import BrightWeb.DisplayUserHelper, only: [encrypt_user_name: 1]
   import BrightWeb.SearchLive.ResultComponents
   import BrightWeb.PathHelper
+  import BrightWeb.ProfileComponents, only: [profile_small_inline: 1]
 
   import BrightWeb.SkillPanelLive.SkillPanelHelper,
     only: [assign_skill_score_dict: 1, assign_counter: 1]
@@ -18,95 +20,108 @@ defmodule BrightWeb.SearchLive.SearchResultComponent do
       do: Sentry.capture_message("BrightWeb.SearchLive.SearchResultComponent: データが表示できません")
 
     ~H"""
-    <div class="flex">
-      <%= if is_nil(@skill_class_score) do %>
-      <div class="bg-white w-[450px] flex items-center justify-center">
-        <p class="text-start">データが表示できません</p>
-      </div>
+    <div class="flex flex-col">
+      <%= if !@anon && @search do %>
+      <.profile_small_inline
+          user_name={@user.name}
+          title={@user.user_profile.title}
+          icon_file_path={UserProfiles.icon_url(@user.user_profile.icon_file_path)}
+          detail={@user.user_profile.detail}
+      />
       <% else %>
-      <div class="bg-white w-[450px]">
-        <p class="text-left" :if={!@anon && @search}><%= @user.name %></p>
-        <.tab
-          id={"#{@prefix}_skill_search_result_tab_#{@index}"}
-          tabs={@tabs}
-          hidden_footer={true}
-          selected_tab={@selected_tab}
-          target={@myself}
-        />
-
-        <div class="relative">
-          <p class="absolute left-0 ml-1 mt-1 top-0">
-            クラス<%= @selected_skill_panel.class %>
-          </p>
-
-          <div class="flex">
-            <div class="mt-4 w-64">
-                <.skill_gem
-                  data={@skill_gem_data}
-                  id={"#{@prefix}-skill-gem-#{@index}"}
-                  labels={@skill_gem_labels}
-                  size="sm"
-                />
-            </div>
-            <.doughnut_area
-              index={"#{@prefix}_#{@index}"}
-              counter={@counter}
-              num_skills={@num_skills}
-              skill_class_score={@skill_class_score}
-            />
-          </div>
-        </div>
-      </div>
+      <.profile_small_inline
+          user_name={"非公開"}
+          icon_file_path={"/images/avatar.png"}
+      />
       <% end %>
-      <div class="border-l border-brightGray-200 border-dashed w-[512px] ml-2 px-2">
-        <div class="flex">
-          <.job_area job={@user.user_job_profile} last_updated={@user.last_updated} />
-          <div :if={@search}>
-          <.action_area
-            user={@user}
-            anon={@anon}
-            skill_panel={@selected_skill_panel}
-            stock_user_ids={@stock_user_ids}
+      <div class="flex border border-brightGray-200 min-h-64 max-h-76 mb-2 overflow-hidden p-2 rounded">
+        <%= if is_nil(@skill_class_score) do %>
+        <div class="bg-white w-[450px] flex items-center justify-center">
+          <p class="text-start">データが表示できません</p>
+        </div>
+        <% else %>
+        <div class="bg-white w-[450px]">
+          <.tab
+            id={"#{@prefix}_skill_search_result_tab_#{@index}"}
+            tabs={@tabs}
+            hidden_footer={true}
+            selected_tab={@selected_tab}
+            target={@myself}
           />
+
+          <div class="relative">
+            <p class="absolute left-0 ml-1 mt-1 top-0">
+              クラス<%= @selected_skill_panel.class %>
+            </p>
+
+            <div class="flex">
+              <div class="mt-4 w-64">
+                  <.skill_gem
+                    data={@skill_gem_data}
+                    id={"#{@prefix}-skill-gem-#{@index}"}
+                    labels={@skill_gem_labels}
+                    size="sm"
+                  />
+              </div>
+              <.doughnut_area
+                index={"#{@prefix}_#{@index}"}
+                counter={@counter}
+                num_skills={@num_skills}
+                skill_class_score={@skill_class_score}
+              />
+            </div>
           </div>
         </div>
-        <div :if={@search} class="flex justify-end mt-8">
-          <!--- β opacity-50 -> hover:opacity-50 に戻すこと --->
-          <a
-            phx-click={
-              if @hr_enabled,
-              do: JS.show(to: "#create_interview_modal") |> JS.push("open", value: %{user: @user.id, skill_params: @skill_params}, target: "#create_interview_modal"),
-              else: JS.push("open_free_trial",target: @myself)
-            }
-            class="self-center bg-base border border-solid border-brightGray-300 cursor-pointer font-bold px-4 py-2 rounded select-none text-center text-white w-56 hover:opacity-50"
-          >
-          面談の打診
-          </a>
-          <a :if={false} class="bg-brightGray-900 border border-solid border-brightGray-300 cursor-pointer font-bold px-4 py-2 rounded select-none text-center text-white w-56 opacity-50">
-          採用・育成チームに採用依頼 <br />βリリース（11月予定）から
-          </a>
-        </div>
-        <div :if={!@search} class="flex justify-start mt-8">
-          <.link
-            class="bg-white text-sm block border border-solid border-brightGreen-300 cursor-pointer font-bold lg:mx-2 my-1 py-1 rounded text-center select-none text-brightGreen-300 w-28 hover:opacity-50"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={
-              skill_panel_path("graphs",%{id: @selected_skill_panel.skill_panel}, %{name_encrypted: encrypt_user_name(@user), name: @user.name},false,@anon)
-              <> "?class=#{@selected_skill_panel.class}"
-            }
-          >
-            成長パネル
-          </.link>
-          <.link
-            class="bg-white text-sm block border border-solid border-brightGreen-300 cursor-pointer font-bold lg:mx-2 my-1 py-1 rounded text-center select-none text-brightGreen-300 w-28 hover:opacity-50"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={if @anon, do: "/mypage/anon/#{encrypt_user_name(@user)}", else: "/mypage/#{@user.name}"}
-          >
-            保有スキル
-          </.link>
-
+        <% end %>
+        <div class="border-l border-brightGray-200 border-dashed w-[512px] ml-2 px-2">
+          <div class="flex">
+            <.job_area job={@user.user_job_profile} last_updated={@user.last_updated} />
+            <div :if={@search}>
+            <.action_area
+              user={@user}
+              anon={@anon}
+              skill_panel={@selected_skill_panel}
+              stock_user_ids={@stock_user_ids}
+            />
+            </div>
+          </div>
+          <div :if={@search} class="flex justify-end mt-8">
+            <!--- β opacity-50 -> hover:opacity-50 に戻すこと --->
+            <a
+              phx-click={
+                if @hr_enabled,
+                do: JS.show(to: "#create_interview_modal") |> JS.push("open", value: %{user: @user.id, skill_params: @skill_params}, target: "#create_interview_modal"),
+                else: JS.push("open_free_trial",target: @myself)
+              }
+              class="self-center bg-base border border-solid border-brightGray-300 cursor-pointer font-bold px-4 py-2 rounded select-none text-center text-white w-56 hover:opacity-50"
+            >
+            面談の打診
+            </a>
+            <a :if={false} class="bg-brightGray-900 border border-solid border-brightGray-300 cursor-pointer font-bold px-4 py-2 rounded select-none text-center text-white w-56 opacity-50">
+            採用・育成チームに採用依頼 <br />βリリース（11月予定）から
+            </a>
+          </div>
+          <div :if={!@search} class="flex justify-start mt-8">
+            <.link
+              class="bg-white text-sm block border border-solid border-brightGreen-300 cursor-pointer font-bold lg:mx-2 my-1 py-1 rounded text-center select-none text-brightGreen-300 w-28 hover:opacity-50"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={
+                skill_panel_path("graphs",%{id: @selected_skill_panel.skill_panel}, %{name_encrypted: encrypt_user_name(@user), name: @user.name},false,@anon)
+                <> "?class=#{@selected_skill_panel.class}"
+              }
+            >
+              成長パネル
+            </.link>
+            <.link
+              class="bg-white text-sm block border border-solid border-brightGreen-300 cursor-pointer font-bold lg:mx-2 my-1 py-1 rounded text-center select-none text-brightGreen-300 w-28 hover:opacity-50"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={if @anon, do: "/mypage/anon/#{encrypt_user_name(@user)}", else: "/mypage/#{@user.name}"}
+            >
+              保有スキル
+            </.link>
+          </div>
         </div>
       </div>
     </div>
