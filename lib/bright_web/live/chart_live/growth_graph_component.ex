8314a92e6b3e5e2561a163e04898083a487e9a1d
koyo-miyamura <nexus9071@gmail.com>
2024-04-07 05:38:32 +0900

    タイムラインと成長グラフがズレてたので修正

--- lib/bright_web/live/chart_live/growth_graph_component.ex
+++ lib/bright_web/live/chart_live/growth_graph_component.ex
@@ -59,7 +59,7 @@ defmodule BrightWeb.ChartLive.GrowthGraphComponent do
       </div>
 
       <% # 成長グラフ %>
-      <div class="flex">
+      <div class="flex pl-7 lg:pl-14">
         <div class="hidden lg:block">
           <.growth_graph data={@data} id="growth-graph"/>
         </div>
