dc07635343a09b3d7aa5603c821d67e44d933ccd
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-04-11 16:30:21 +0900

    get_next_percentageを作成

--- lib/bright_web/components/level_components.ex
+++ lib/bright_web/components/level_components.ex
@@ -5,6 +5,11 @@ defmodule BrightWeb.LevelComponents do
   use Phoenix.Component
   alias Bright.SkillScores
 
+  # レベルの判定値
+  @normal_level 40
+  @skilled_level 60
+  @master_level 100
+
   @doc """
   Renders a Level
   ## Examples
@@ -18,34 +23,28 @@ defmodule BrightWeb.LevelComponents do
   attr :size, :integer
 
   def level(assigns) do
-    IO.inspect("--------------")
-    # IO.inspect(assigns)
-    # SkillScores.calc_high_skills_percentage(assigns.value, assigns.size)
-    # |> IO.inspect()
-    # |> get_level()
-    # |> IO.inspect()
+    percentage = SkillScores.calc_high_skills_percentage(assigns.value, assigns.size)
+    level = get_level(percentage)
 
     assigns =
       assigns
-      |> assign(
-        level: SkillScores.calc_high_skills_percentage(assigns.value, assigns.size) |> get_level()
-      )
+      |> assign(level: level)
+      |> assign(next_percentage: get_next_percentage(level, percentage))
 
     ~H"""
     <div class="flex justify-center">
       <.level_render
        level={@level}
+       next_percentage={@next_percentage}
       />
     </div>
     """
   end
 
   defp level_render(%{level: :beginner} = assigns) do
-    IO.inspect(assigns)
-
     ~H"""
     <p>
-    あと<span class="text-error !text-2xl font-bold">8</span>%で平均になれます。
+    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%で平均になれます。
     </p>
     <p>
       次のレベルまでのスキル数<span
@@ -56,11 +55,9 @@ defmodule BrightWeb.LevelComponents do
   end
 
   defp level_render(%{level: :normal} = assigns) do
-    IO.inspect(assigns)
-
     ~H"""
     <p>
-    あと<span class="text-error !text-2xl font-bold">8</span>%でベテランになれます。
+    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でベテランになれます。
     </p>
     <p>
       次のレベルまでのスキル数<span
@@ -71,11 +68,9 @@ defmodule BrightWeb.LevelComponents do
   end
 
   defp level_render(%{level: :skilled} = assigns) do
-    IO.inspect(assigns)
-
     ~H"""
     <p>
-    あと<span class="text-error !text-2xl font-bold">8</span>%でマスターになれます。
+    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でマスターになれます。
     </p>
     <p>
       マスターまでのスキル数<span
@@ -86,8 +81,6 @@ defmodule BrightWeb.LevelComponents do
   end
 
   defp level_render(%{level: :master} = assigns) do
-    IO.inspect(assigns)
-
     ~H"""
     <p>
      このスキルはマスターしました。
@@ -97,6 +90,11 @@ defmodule BrightWeb.LevelComponents do
     </p>
     """
   end
+
+  defp get_next_percentage(:beginner, percentage), do: @normal_level - percentage
+  defp get_next_percentage(:normal, percentage), do: @skilled_level - percentage
+  defp get_next_percentage(:skilled, percentage), do: @master_level - percentage
+  defp get_next_percentage(:master, _percentage), do: 0
 
   defp get_level(100), do: :master
   defp get_level(percentage), do: SkillScores.get_level(percentage)
