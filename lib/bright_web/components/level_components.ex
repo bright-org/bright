fff6e2f19de13fb97170703f2fd2ecfd59a6472d
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-04-11 15:50:43 +0900

    level_renderを作成

--- lib/bright_web/components/level_components.ex
+++ lib/bright_web/components/level_components.ex
@@ -20,19 +20,44 @@ defmodule BrightWeb.LevelComponents do
   def level(assigns) do
     IO.inspect("--------------")
     # IO.inspect(assigns)
-    SkillScores.calc_high_skills_percentage(assigns.value, assigns.size)
-    |> IO.inspect()
-    |> get_level()
-    |> IO.inspect()
+    # SkillScores.calc_high_skills_percentage(assigns.value, assigns.size)
+    # |> IO.inspect()
+    # |> get_level()
+    # |> IO.inspect()
+
+    assigns =
+      assigns
+      |> assign(
+        level: SkillScores.calc_high_skills_percentage(assigns.value, assigns.size) |> get_level()
+      )
 
     ~H"""
     <div class="flex justify-center">
-      <.level_render />
+      <.level_render
+       level={@level}
+      />
     </div>
     """
   end
 
-  defp level_render(assigns) do
+  defp level_render(%{level: :beginner} = assigns) do
+    IO.inspect(assigns)
+
+    ~H"""
+    <p>
+    あと<span class="text-error !text-2xl font-bold">8</span>%で平均になれます。
+    </p>
+    <p>
+      次のレベルまでのスキル数<span
+        class="text-error !text-2xl font-bold"
+        >10</span>個
+    </p>
+    """
+  end
+
+  defp level_render(%{level: :normal} = assigns) do
+    IO.inspect(assigns)
+
     ~H"""
     <p>
     あと<span class="text-error !text-2xl font-bold">8</span>%でベテランになれます。
@@ -41,6 +66,34 @@ defmodule BrightWeb.LevelComponents do
       次のレベルまでのスキル数<span
         class="text-error !text-2xl font-bold"
         >10</span>個
+    </p>
+    """
+  end
+
+  defp level_render(%{level: :skilled} = assigns) do
+    IO.inspect(assigns)
+
+    ~H"""
+    <p>
+    あと<span class="text-error !text-2xl font-bold">8</span>%でマスターになれます。
+    </p>
+    <p>
+      マスターまでのスキル数<span
+        class="text-error !text-2xl font-bold"
+        >10</span>個
+    </p>
+    """
+  end
+
+  defp level_render(%{level: :master} = assigns) do
+    IO.inspect(assigns)
+
+    ~H"""
+    <p>
+     このスキルはマスターしました。
+    </p>
+    <p>
+     おめでとうございます
     </p>
     """
   end
