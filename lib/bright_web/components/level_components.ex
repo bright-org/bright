71b9e2202bc58487f433a476729153a3680ab8dd
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-04-11 12:25:57 +0900

    get_levelの作成

--- lib/bright_web/components/level_components.ex
+++ lib/bright_web/components/level_components.ex
@@ -3,6 +3,7 @@ defmodule BrightWeb.LevelComponents do
   Level Components
   """
   use Phoenix.Component
+  alias Bright.SkillScores
 
   @doc """
   Renders a Level
@@ -18,19 +19,32 @@ defmodule BrightWeb.LevelComponents do
 
   def level(assigns) do
     IO.inspect("--------------")
-    IO.inspect(assigns)
+    # IO.inspect(assigns)
+    SkillScores.calc_high_skills_percentage(assigns.value, assigns.size)
+    |> IO.inspect()
+    |> get_level()
+    |> IO.inspect()
 
     ~H"""
     <div class="flex justify-center">
-      <p>
-        あと<span class="text-error !text-2xl font-bold">8</span>%でベテランになれます。
-      </p>
-      <p>
-        次のレベルまでのスキル数<span
-          class="text-error !text-2xl font-bold"
-          >10</span>個
-      </p>
+      <.level_render />
     </div>
     """
   end
+
+  defp level_render(assigns) do
+    ~H"""
+    <p>
+    あと<span class="text-error !text-2xl font-bold">8</span>%でベテランになれます。
+    </p>
+    <p>
+      次のレベルまでのスキル数<span
+        class="text-error !text-2xl font-bold"
+        >10</span>個
+    </p>
+    """
+  end
+
+  defp get_level(100), do: :master
+  defp get_level(percentage), do: SkillScores.get_level(percentage)
 end
