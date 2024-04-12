4bd347f56de48bfb02d21d00b120ff403989e60a
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-04-12 17:30:11 +0900

    SP対応

--- lib/bright_web/components/level_components.ex
+++ lib/bright_web/components/level_components.ex
@@ -33,7 +33,7 @@ defmodule BrightWeb.LevelComponents do
       |> assign(next_num_skills: get_next_num_skills(level, assigns.size, assigns.value))
 
     ~H"""
-    <div class="flex">
+    <div class="flex px-10">
       <.level_render
        level={@level}
        next_percentage={@next_percentage}
@@ -45,51 +45,53 @@ defmodule BrightWeb.LevelComponents do
 
   defp level_render(%{level: :beginner} = assigns) do
     ~H"""
-    <p>
-    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%で平均になれます。
-    </p>
-    <p>
-      次のレベルまでのスキル数<span
-        class="text-error !text-2xl font-bold"
-        ><%= @next_num_skills %></span>個
-    </p>
+    <div class="flex flex-col lg:flex-row">
+     <div>
+      あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%で平均になれます。
+     </div>
+     <div>
+      次のレベルまでのスキル数<span class="text-error !text-2xl font-bold" ><%= @next_num_skills %></span>個
+      </div>
+    </div>
     """
   end
 
   defp level_render(%{level: :normal} = assigns) do
     ~H"""
-    <p>
-    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でベテランになれます。
-    </p>
-    <p>
-      次のレベルまでのスキル数<span
-        class="text-error !text-2xl font-bold"
-        ><%= @next_num_skills %></span>個
-    </p>
+    <div class="flex flex-col lg:flex-row">
+      <div>
+        あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でベテランになれます。
+      </div>
+      <div>
+        次のレベルまでのスキル数<span class="text-error !text-2xl font-bold"><%= @next_num_skills %></span>個
+      </div>
+    </div>
     """
   end
 
   defp level_render(%{level: :skilled} = assigns) do
     ~H"""
-    <p>
-    あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でマスターになれます。
-    </p>
-    <p>
-      マスターまでのスキル数<span
-        class="text-error !text-2xl font-bold"
-        ><%= @next_num_skills %></span>個
-    </p>
+    <div class="flex flex-col lg:flex-row">
+      <div>
+        あと<span class="text-error !text-2xl font-bold"><%= @next_percentage %></span>%でマスターになれます。
+      </div>
+      <div>
+        マスターまでのスキル数<span class="text-error !text-2xl font-bold"><%= @next_num_skills %></span>個
+      </div>
+    </div>
     """
   end
 
   defp level_render(%{level: :master} = assigns) do
     ~H"""
-    <p>
-     このスキルはマスターしました。
-    </p>
-    <p>
-     おめでとうございます
-    </p>
+    <div class="flex flex-col lg:flex-row">
+      <div>
+        このスキルはマスターしました。
+      </div>
+      <div>
+        おめでとうございます
+      </div>
+    </div>
     """
   end
 
@@ -98,9 +100,15 @@ defmodule BrightWeb.LevelComponents do
   defp get_next_percentage(:skilled, percentage), do: @master_level - percentage
   defp get_next_percentage(:master, _percentage), do: 0
 
-  defp get_next_num_skills(:beginner, num_skills, current_skills), do: ceil((num_skills * (@normal_level / 100)) - current_skills)
-  defp get_next_num_skills(:normal, num_skills, current_skills), do: ceil((num_skills * (@skilled_level / 100)) - current_skills)
-  defp get_next_num_skills(:skilled, num_skills, current_skills), do: ceil(num_skills  - current_skills)
+  defp get_next_num_skills(:beginner, num_skills, current_skills),
+    do: ceil(num_skills * (@normal_level / 100) - current_skills)
+
+  defp get_next_num_skills(:normal, num_skills, current_skills),
+    do: ceil(num_skills * (@skilled_level / 100) - current_skills)
+
+  defp get_next_num_skills(:skilled, num_skills, current_skills),
+    do: ceil(num_skills - current_skills)
+
   defp get_next_num_skills(:master, _num_skills, _current_skills), do: 0
 
   defp get_level(100), do: :master
