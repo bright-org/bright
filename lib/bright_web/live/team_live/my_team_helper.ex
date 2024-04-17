f2180a7d0a8b0ec2805cef5e763a63c1207c009f
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-04-17 17:56:30 +0900

    レベル計算試作

--- lib/bright_web/live/team_live/my_team_helper.ex
+++ lib/bright_web/live/team_live/my_team_helper.ex
@@ -54,6 +54,11 @@ defmodule BrightWeb.TeamLive.MyTeamHelper do
     member_skill_class_scores = list_skill_class_scores(display_team_members, display_skill_panel)
     subscription = Subscriptions.get_user_subscription_user_plan(user.id)
 
+    level = [:beginner, :normal, :skilled]
+    IO.inspect("-----------------------------------------------------")
+    Enum.map(1..3, fn c ->  Enum.map(level, fn x ->  level_count(member_skill_class_scores, c, x) end) end)
+    |> IO.inspect()
+
     # スキルとチームの取得結果に応じて各種assign
     socket
     |> assign(:show_hr_support_modal, false)
@@ -75,6 +80,12 @@ defmodule BrightWeb.TeamLive.MyTeamHelper do
     # パラメータの指定内容とデータの取得結果によってリダイレクトを指定
     |> assign_push_redirect(params, display_team, display_skill_panel)
     |> assign(:team_size, length(display_team_members))
+  end
+
+  defp level_count(member_skill_class_scores, class, level) do
+    member_skill_class_scores
+    |> Enum.filter(fn x -> x.skill_class.class == class and x.level == level end)
+    |> Enum.count()
   end
 
   defp get_display_skill_panel(%{"skill_panel_id" => skill_panel_id}, _display_team_members) do
