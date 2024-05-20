00963304085276085196c999854a6c8885b691dd
thehaigo <sirannha@gmail.com>
2024-05-20 11:48:52 +0900

    1on1開始のイベントを面談打診からチャット作成に変更

--- lib/bright_web/live/team_live/team_member_skill_card_component.ex
+++ lib/bright_web/live/team_live/team_member_skill_card_component.ex
@@ -11,180 +11,171 @@ defmodule BrightWeb.TeamMemberSkillCardComponent do
 
   alias Bright.UserProfiles
   alias Bright.SkillPanels
+  alias Bright.UserSearches
+  alias Bright.Chats
+  alias Bright.Recruits
+  alias Bright.Recruits.Interview
 
   @impl true
   def render(assigns) do
     ~H"""
-      <div id={@id} class="flex flex-col justify-between w-full lg:w-[calc(50%-8px)] 2xl:w-[calc(33.33333%-11px)] lg:min-w-[474px] h-[544px] lg:h-[654px] shadow bg-white relative">
-        <.team_member_class_tab
-          user={@display_skill_card.user}
-          user_skill_class_score={@display_skill_card.user_skill_class_score}
-          select_skill_class={@display_skill_card.select_skill_class}
-          skill_class_tab_click_target={@myself}
-        />
+    <div
+      id={@id}
+      class="flex flex-col justify-between w-full lg:w-[calc(50%-8px)] 2xl:w-[calc(33.33333%-11px)] lg:min-w-[474px] h-[544px] lg:h-[654px] shadow bg-white relative"
+    >
+      <.team_member_class_tab
+        user={@display_skill_card.user}
+        user_skill_class_score={@display_skill_card.user_skill_class_score}
+        select_skill_class={@display_skill_card.select_skill_class}
+        skill_class_tab_click_target={@myself}
+      />
 
-        <% me = @display_skill_card.user.id == @current_user.id %>
-        <div class="flex pt-2 px-4 h-20">
-          <div class="flex w-full flex-row text-xl items-center justify-between">
-            <div class="flex items-center">
-              <div>
-                <img
-                  class="object-cover inline-block mr-2 lg:mr-5 h-[42px] w-[42px] lg:h-16 lg:w-16 rounded-full"
-                  src={UserProfiles.icon_url(assigns.display_skill_card.user.user_profile.icon_file_path)}
-                />
-              </div>
-              <.link
-                class="text-xl w-40 lg:w-56 truncate lg:text-2xl font-bold hover:opacity-50"
-                navigate={~p"/mypage/#{if me, do: "", else: @display_skill_card.user.name}"}
-              >
-                <%= @display_skill_card.user.name %>
-              </.link>
+      <% me = @display_skill_card.user.id == @current_user.id %>
+      <div class="flex pt-2 px-4 h-20">
+        <div class="flex w-full flex-row text-xl items-center justify-between">
+          <div class="flex items-center">
+            <div>
+              <img
+                class="object-cover inline-block mr-2 lg:mr-5 h-[42px] w-[42px] lg:h-16 lg:w-16 rounded-full"
+                src={
+                  UserProfiles.icon_url(assigns.display_skill_card.user.user_profile.icon_file_path)
+                }
+              />
             </div>
-            <div class="flex gap-x-1 lg:gap-x-2">
-              <.link
-                  :if={ !is_nil(@display_skill_card.user_skill_class_score)}
-                  class="h-8 bg-white flex items-center justify-center border border-solid border-brightGreen-300 px-1 rounded text-center hover:opacity-50"
-                  href={
+            <.link
+              class="text-xl w-40 lg:w-56 truncate lg:text-2xl font-bold hover:opacity-50"
+              navigate={~p"/mypage/#{if me, do: "", else: @display_skill_card.user.name}"}
+            >
+              <%= @display_skill_card.user.name %>
+            </.link>
+          </div>
+          <div class="flex gap-x-1 lg:gap-x-2">
+            <.link
+              :if={!is_nil(@display_skill_card.user_skill_class_score)}
+              class="h-8 bg-white flex items-center justify-center border border-solid border-brightGreen-300 px-1 rounded text-center hover:opacity-50"
+              href={
                     skill_panel_path("graphs",@display_skill_panel, @display_skill_card.user, me, false)
                     <> "?class=#{@display_skill_card.select_skill_class.class}"
                   }
-              >
-                <div
-                  class={
-                    "inline-block h-6 w-6 [mask-image:url('/images/common/icons/growthPanel.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-brightGreen-300"
-                  }
-                />
-              </.link>
+            >
+              <div class="inline-block h-6 w-6 [mask-image:url('/images/common/icons/growthPanel.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-brightGreen-300" />
+            </.link>
 
-              <.link
-                  :if={ !is_nil(@display_skill_card.user_skill_class_score)}
-                  class="h-8 bg-white flex items-center justify-center border border-solid border-brightGreen-300 px-1 rounded text-center hover:opacity-50"
-                  href={
+            <.link
+              :if={!is_nil(@display_skill_card.user_skill_class_score)}
+              class="h-8 bg-white flex items-center justify-center border border-solid border-brightGreen-300 px-1 rounded text-center hover:opacity-50"
+              href={
                     skill_panel_path("panels",@display_skill_panel, @display_skill_card.user, me, false)
                     <> "?class=#{@display_skill_card.select_skill_class.class}"
                   }
-              >
-                <div
-                  class={
-                    "inline-block h-6 w-6 [mask-image:url('/images/common/icons/skillPanel.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-brightGreen-300"
-                  }
-                />
-              </.link>
-            </div>
+            >
+              <div class="inline-block h-6 w-6 [mask-image:url('/images/common/icons/skillPanel.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-brightGreen-300" />
+            </.link>
           </div>
         </div>
+      </div>
 
-        <div class="px-4 text-base break-words">
-          <%= @display_skill_card.user.user_profile.title %>
-        </div>
+      <div class="px-4 text-base break-words">
+        <%= @display_skill_card.user.user_profile.title %>
+      </div>
 
-        <div
-          :if={is_nil(@display_skill_card.user_skill_class_score)}
-          class="w-full lg:w-[400px] h-[240px] mt-12 lg:mt-0 lg:h-[400px] flex justify-center mx-auto"
-          >
-          <p
-            class="font-bold inline-block align-middle my-auto mx-auto justify-center"
-          >
-            スキル保有していません
-          </p>
-        </div>
+      <div
+        :if={is_nil(@display_skill_card.user_skill_class_score)}
+        class="w-full lg:w-[400px] h-[240px] mt-12 lg:mt-0 lg:h-[400px] flex justify-center mx-auto"
+      >
+        <p class="font-bold inline-block align-middle my-auto mx-auto justify-center">
+          スキル保有していません
+        </p>
+      </div>
 
-        <div
-          :if={!is_nil(@display_skill_card.user_skill_class_score)}
-          class="hidden lg:flex w-[400px] h-[400px] justify-center mx-auto"
-          >
-          <.live_component
-            id={"skill-gem-#{@id}"}
-            module={BrightWeb.ChartLive.SkillGemComponent}
-            display_user={@display_skill_card.user}
-            skill_panel={@display_skill_panel}
-            class={@display_skill_card.select_skill_class.class}
-            select_label={"now"}
-            me={:true}
-            anonymous={:false}
-            root={""}
-            size="md"
-            display_link="false"
-          />
-        </div>
+      <div
+        :if={!is_nil(@display_skill_card.user_skill_class_score)}
+        class="hidden lg:flex w-[400px] h-[400px] justify-center mx-auto"
+      >
+        <.live_component
+          id={"skill-gem-#{@id}"}
+          module={BrightWeb.ChartLive.SkillGemComponent}
+          display_user={@display_skill_card.user}
+          skill_panel={@display_skill_panel}
+          class={@display_skill_card.select_skill_class.class}
+          select_label="now"
+          me={true}
+          anonymous={false}
+          root=""
+          size="md"
+          display_link="false"
+        />
+      </div>
 
-        <div
-          :if={!is_nil(@display_skill_card.user_skill_class_score)}
-          class="lg:hidden w-full h-[300px] flex justify-center mx-auto"
-          >
-          <.live_component
-            id={"skill-gem-sp-#{@id}"}
-            module={BrightWeb.ChartLive.SkillGemComponent}
-            display_user={@display_skill_card.user}
-            skill_panel={@display_skill_panel}
-            class={@display_skill_card.select_skill_class.class}
-            select_label={"now"}
-            me={:true}
-            anonymous={:false}
-            root={""}
-            size="sp"
-            display_link="false"
-          />
-        </div>
+      <div
+        :if={!is_nil(@display_skill_card.user_skill_class_score)}
+        class="lg:hidden w-full h-[300px] flex justify-center mx-auto"
+      >
+        <.live_component
+          id={"skill-gem-sp-#{@id}"}
+          module={BrightWeb.ChartLive.SkillGemComponent}
+          display_user={@display_skill_card.user}
+          skill_panel={@display_skill_panel}
+          class={@display_skill_card.select_skill_class.class}
+          select_label="now"
+          me={true}
+          anonymous={false}
+          root=""
+          size="sp"
+          display_link="false"
+        />
+      </div>
 
-
-        <div class="pb-2 flex w-full gap-x-1 lg:gap-x-2 justify-around">
-          <button
-            :if={@display_skill_card.user.id != @current_user.id}
-            class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-base hover:opacity-50"
-            phx-click={
-                if @hr_enabled,
-                do: JS.show(to: "#create_interview_modal") |> JS.push("open", value: %{user: @display_skill_card.user.id, skill_params: [%{skill_panel: @display_skill_panel.id, career_field: "1on1"}]}, target: "#create_interview_modal")
-              }
-          >
-            <div
-              class={
-                "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/oneOnOneInvitation.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white"
-              }
-            />
-            1on1に誘う
-          </button>
-          <%= if @display_skill_card.user.id == @current_user.id do %>
-            <%= if !is_nil(@display_skill_panel) && is_nil(@display_skill_card.user_skill_class_score) do %>
-              <.link navigate={~p"/more_skills/teams/#{@team_id}/skill_panels/#{@display_skill_panel}"}>
-              <button class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-base hover:opacity-50">
-                <div
-                  class={[
-                    "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/skillSelect.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white"]
+      <div class="pb-2 flex w-full gap-x-1 lg:gap-x-2 justify-around">
+        <button
+          :if={@display_skill_card.user.id != @current_user.id}
+          class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-base hover:opacity-50"
+          phx-click={
+            if @hr_enabled,
+              do:
+                JS.push("start_1on1",
+                  target: @myself,
+                  value: %{
+                    user_id: @display_skill_card.user.id,
+                    skill_params: [%{skill_panel: @display_skill_panel.id, career_field: "1on1"}]
                   }
-                />
-                スキルを取得
+                )
+          }
+        >
+          <div class="inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/oneOnOneInvitation.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white" />
+          1on1に誘う
+        </button>
+        <%= if @display_skill_card.user.id == @current_user.id do %>
+          <%= if !is_nil(@display_skill_panel) && is_nil(@display_skill_card.user_skill_class_score) do %>
+            <.link navigate={~p"/more_skills/teams/#{@team_id}/skill_panels/#{@display_skill_panel}"}>
+              <button class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-base hover:opacity-50">
+                <div class={[
+                  "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/skillSelect.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white"
+                ]} /> スキルを取得
               </button>
-              </.link>
-            <% end %>
-          <% else %>
-            <.link
-              :if={comparable_skill_panel?(@display_skill_panel, @display_skill_card, @current_user)}
-              href={
+            </.link>
+          <% end %>
+        <% else %>
+          <.link
+            :if={comparable_skill_panel?(@display_skill_panel, @display_skill_card, @current_user)}
+            href={
                 skill_panel_path("graphs", @display_skill_panel, @current_user, true, false)
                 <> "?class=#{@display_skill_card.select_skill_class.class}&compare=#{@display_skill_card.user.name}"
               }
-            >
-              <.action_button class="flex gap-x-1 lg:gap-x-2 items-center px-1 lg:px-3 py-2">
-                <div
-                  class={[
-                    "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/switchIndividual.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-base"]
-                  }
-                />
-                この人と比較
-              </.action_button>
-            </.link>
-          <% end %>
-          <button class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-brightGray-200">
-            <div
-              class={
-                "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/skillUp.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white"
-              }
-            />
-            スキルアップ確認
-          </button>
-        </div>
+          >
+            <.action_button class="flex gap-x-1 lg:gap-x-2 items-center px-1 lg:px-3 py-2">
+              <div class={[
+                "inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/switchIndividual.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-base"
+              ]} /> この人と比較
+            </.action_button>
+          </.link>
+        <% end %>
+        <button class="flex gap-x-1 lg:gap-x-2 items-center text-xs lg:text-sm font-bold px-1 lg:px-3 py-2 rounded text-white bg-brightGray-200">
+          <div class="inline-block h-4 w-4 lg:h-6 lg:w-6 [mask-image:url('/images/common/icons/skillUp.svg')] [mask-position:center_center] [mask-size:100%] [mask-repeat:no-repeat] bg-white" />
+          スキルアップ確認
+        </button>
       </div>
+    </div>
     """
   end
 
@@ -222,11 +213,69 @@ defmodule BrightWeb.TeamMemberSkillCardComponent do
     {:noreply, socket}
   end
 
+  def handle_event("start_1on1", %{"skill_params" => skill_params, "user_id" => user_id}, socket) do
+    recruiter = socket.assigns.current_user
+
+    interview =
+      case Recruits.get_interview(recruiter.id, user_id) do
+        %Interview{} = interview ->
+          interview
+
+        nil ->
+          skill_params =
+            skill_params
+            |> Enum.map(
+              &(Enum.map(&1, fn {k, v} -> {String.to_atom(k), v} end)
+                |> Enum.into(%{}))
+            )
+
+          candidates_user =
+            UserSearches.get_user_by_id_with_job_profile_and_skill_score(user_id, skill_params)
+            |> List.first()
+
+          interview_params = %{
+            "status" => :one_on_one,
+            "skill_panel_name" => gen_interview_name(skill_params),
+            "desired_income" => candidates_user.desired_income,
+            "skill_params" => Jason.encode!(skill_params),
+            "interview_members" => [],
+            "recruiter_user_id" => recruiter.id,
+            "candidates_user_id" => candidates_user.id
+          }
+
+          {:ok, interview} = Recruits.create_interview(interview_params)
+
+          interview
+      end
+
+    chat =
+      Chats.get_or_create_chat(
+        interview.recruiter_user_id,
+        interview.candidates_user_id,
+        interview.id,
+        "recruit",
+        [
+          %{user_id: interview.recruiter_user_id},
+          %{user_id: interview.candidates_user_id}
+        ]
+      )
+
+    {:noreply, push_navigate(socket, to: ~p"/recruits/chats/#{chat.id}")}
+  end
+
   defp comparable_skill_panel?(skill_panel, skill_card, current_user) do
     # 「この人と比較」押下可能かどうか
     # - 「この人」にスキルスコアがあり、
     # - 自身が取得済みのスキルパネルであること
     skill_card.user_skill_class_score &&
       SkillPanels.get_user_skill_panel(current_user, skill_panel.id)
+  end
+
+  defp gen_interview_name(skill_params) do
+    skill_params
+    |> List.first()
+    |> Map.get(:skill_panel)
+    |> SkillPanels.get_skill_panel!()
+    |> Map.get(:name)
   end
 end
