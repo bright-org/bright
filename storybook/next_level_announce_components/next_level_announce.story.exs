4bbf9c83800ce4a1a9f806063e77b02083c08652
t-yamanashi <ae86bg5@yahoo.co.jp>
2024-05-22 13:19:51 +0900

    Storybookを修正

--- storybook/next_level_announce_components/next_level_announce.story.exs
+++ storybook/next_level_announce_components/next_level_announce.story.exs
@@ -9,28 +9,36 @@ defmodule Storybook.Components.NextLevelAnnounce do
         id: :value39,
         attributes: %{
           value: 39,
-          size: 100
+          size: 100,
+          skill_panel_id: "",
+          me: true
         }
       },
       %Variation{
         id: :value59,
         attributes: %{
           value: 59,
-          size: 100
+          size: 100,
+          skill_panel_id: "",
+          me: true
         }
       },
       %Variation{
         id: :value99,
         attributes: %{
           value: 99,
-          size: 100
+          size: 100,
+          skill_panel_id: "",
+          me: false
         }
       },
       %Variation{
         id: :value100,
         attributes: %{
           value: 100,
-          size: 100
+          size: 100,
+          skill_panel_id: "",
+          me: false
         }
       }
     ]
