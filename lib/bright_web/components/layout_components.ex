8a7f99cbbfab387b694163637e1b1c7d7c2eef41
thehaigo <sirannha@gmail.com>
2024-05-22 15:11:45 +0900

    フォーマットがかかってエラーになってたのでもとに戻す

--- lib/bright_web/components/layout_components.ex
+++ lib/bright_web/components/layout_components.ex
@@ -46,7 +46,7 @@ defmodule BrightWeb.LayoutComponents do
         />
         <meta
           property="og:description"
-          content="ITで世の中に価値をもたらすエンジニアやインフラ、デザイナー、マーケッターから、気になるスキルを選び、あなたがどのような"輝き"を放つかを体験してください。"
+          content={"ITで世の中に価値をもたらすエンジニアやインフラ、デザイナー、マーケッターから、気になるスキルを選び、あなたがどのような\"輝き\"を放つかを体験してください。"}
         />
         <meta property="og:image" content="https://bright-fun.org/images/ogp_a.png" />
         <meta property="og:type" content="article" />
