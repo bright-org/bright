0020724cd0740829bf9103a23bf4a51814c55275
thehaigo <sirannha@gmail.com>
2024-05-22 15:11:28 +0900

    復活してたので削除

--- .formatter.exs
+++ .formatter.exs
@@ -2,7 +2,6 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
   inputs: [
     "*.{heex,ex,exs}",
     "{config,lib,test}/**/*.{heex,ex,exs}",
