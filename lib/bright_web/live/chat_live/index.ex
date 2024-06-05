0ba2e39057f40cab5136378799abc05f23ea4be6
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-06-05 17:19:51 +0900

    デフォルト条件変更と面談キャンセル追加

--- lib/bright_web/live/chat_live/index.ex
+++ lib/bright_web/live/chat_live/index.ex
@@ -17,6 +17,7 @@ defmodule BrightWeb.ChatLive.Index do
     %{name: "面談確定待ち", value: :consume_interview},
     %{name: "面談確定", value: :ongoing_interview},
     %{name: "採用選考中", value: :completed_interview},
+    %{name: "面談キャンセル", value: :cancel_interview},
     %{name: "（すべて）", value: :recruit}
   ]
 
