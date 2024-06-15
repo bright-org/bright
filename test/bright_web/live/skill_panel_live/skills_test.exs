e27e5b602991e0e6ecfa13e333f2e229459a68f1
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-06-15 14:46:58 +0900

    パスをpanelsに変更

--- test/bright_web/live/skill_panel_live/skills_test.exs
+++ test/bright_web/live/skill_panel_live/skills_test.exs
@@ -223,7 +223,7 @@ defmodule BrightWeb.SkillPanelLive.SkillsTest do
       conn: conn,
       skill_panel: skill_panel
     } do
-      {:ok, show_live, html} = live(conn, ~p"/graphs/#{skill_panel}")
+      {:ok, show_live, html} = live(conn, ~p"/panels/#{skill_panel}")
 
       assert html =~
                ~s{class="bg-white border border-brightGreen-500 rounded px-1 h-8 flex items-center mt-auto mb-1 hover:filter hover:brightness-95"}
@@ -236,7 +236,7 @@ defmodule BrightWeb.SkillPanelLive.SkillsTest do
       assert html =~
                ~s{class="bg-white border border-brightGreen-300 rounded px-1 h-8 flex items-center mt-auto mb-1 hover:filter hover:brightness-95"}
 
-      {:ok, show_live, html} = live(conn, ~p"/graphs/#{skill_panel}")
+      {:ok, show_live, html} = live(conn, ~p"/panels/#{skill_panel}")
 
       assert html =~
                ~s{class="bg-white border border-brightGreen-300 rounded px-1 h-8 flex items-center mt-auto mb-1 hover:filter hover:brightness-95"}
