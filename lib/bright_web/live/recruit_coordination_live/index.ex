f0bb3070570eb9228916df764029322ca497de60
thehaigo <sirannha@gmail.com>
2024-07-05 14:58:26 +0900

    採用確定したユーザーを採用結果に表示するように変更

--- lib/bright_web/live/recruit_coordination_live/index.ex
+++ lib/bright_web/live/recruit_coordination_live/index.ex
@@ -112,7 +112,7 @@ defmodule BrightWeb.RecruitCoordinationLive.Index do
     <div id="coordination_member_container" class="bg-white rounded-md my-1 mb-20 lg:my-20 lg:w-3/5 m-auto p-5">
       <div class="text-sm font-medium text-center">
         <h4 class="text-start">選考結果</h4>
-        <li :if={Enum.count(@acceptances) == 0} class="flex">
+        <li :if={Enum.count(@acceptances) + Enum.count(@waiting_acceptances) + Enum.count(@acceptance_members)== 0} class="flex">
           <div class="text-left flex items-center text-base py-4 flex-1 mr-2">
             未返答の選考結果はありません
           </div>
@@ -141,6 +141,81 @@ defmodule BrightWeb.RecruitCoordinationLive.Index do
                 <.elapsed_time inserted_at={acceptance.updated_at} />
               </span>
             </.link>
+          </li>
+        <% end %>
+        <%= for coordination <- @waiting_acceptances do %>
+          <% icon_path = coordination.candidates_user.user_profile.icon_file_path %>
+          <li class="flex my-5">
+            <.link
+               patch={~p"/recruits/coordinations/#{coordination.id}"}
+              class="cursor-pointer hover:opacity-70 text-left flex items-center text-base px-1 py-1 flex-1 mr-4 w-full lg:w-auto lg:flex-nowrap truncate"
+            >
+              <div class="flex flex-col">
+                <img
+                  src={UserProfiles.icon_url(icon_path)}
+                  class="object-cover h-12 w-12 rounded-full mr-2"
+                  alt=""
+                />
+                <span><%= coordination.candidates_user.name %></span>
+              </div>
+              <div class="flex-1">
+                <span><%= if coordination.skill_panel_name == nil, do: "スキルパネルデータなし", else: coordination.skill_panel_name %></span>
+                <br />
+                <span class="text-brightGray-300">
+                <%= NaiveDateTime.to_date(coordination.inserted_at) %>
+                希望年収:<%= coordination.desired_income %>
+                </span>
+              </div>
+
+              <span class="flex-1">
+                <%= Gettext.gettext(BrightWeb.Gettext, to_string(coordination.status)) %>
+              </span>
+              <span class="w-24">
+                <.elapsed_time inserted_at={coordination.updated_at} />
+              </span>
+            </.link>
+          </li>
+        <% end %>
+        <%= for member <- @acceptance_members do %>
+        <% icon_path = member.coordination.candidates_user.user_profile.icon_file_path %>
+        <% recruiter = member.coordination.recruiter_user %>
+          <li class="flex my-5">
+              <div class="flex flex-col mr-4">
+                <img
+                  src={UserProfiles.icon_url(recruiter.user_profile.icon_file_path)}
+                  class="object-cover h-12 w-12 rounded-full ml-2"
+                  alt=""
+                />
+                <span><%= recruiter.name %></span>
+              </div>
+
+              <div class="flex flex-col">
+                <img
+                  src={UserProfiles.icon_url(icon_path)}
+                  class="object-cover h-12 w-12 rounded-full mr-2"
+                  alt=""
+                />
+                <span>
+                  <%= member.coordination.candidates_user.name %>
+                </span>
+              </div>
+              <div class="flex-1">
+                <span><%= if member.coordination.skill_panel_name == nil, do: "スキルパネルデータなし", else: member.coordination.skill_panel_name %></span>
+                <br />
+                <span class="text-brightGray-300">
+                  <%= NaiveDateTime.to_date(member.inserted_at) %>
+                  希望年収:<%= member.coordination.desired_income %>
+                </span>
+              </div>
+              <span class="flex-1">
+                <%= Gettext.gettext(BrightWeb.Gettext, to_string(member.coordination.status)) %>
+              </span>
+              <span class="flex-1">
+                <%= Gettext.gettext(BrightWeb.Gettext, to_string(member.decision)) %>
+              </span>
+              <span class="w-24">
+                <.elapsed_time inserted_at={member.updated_at} />
+              </span>
           </li>
         <% end %>
       </div>
@@ -206,11 +281,13 @@ defmodule BrightWeb.RecruitCoordinationLive.Index do
 
     socket
     |> assign(:page_title, "採用の選考状況")
-    |> assign(:coordinations, Recruits.list_coordination(user_id, :not_complete))
+    |> assign(:coordinations, Recruits.list_coordination(user_id, :waiting_recruit_decision))
     |> assign(:coordination_members, Recruits.list_coordination_members(user_id, :not_answered))
     |> assign(:coordination, nil)
     |> assign(:coordination_member, nil)
     |> assign(:acceptances, Recruits.list_acceptance_employment(user_id))
+    |> assign(:waiting_acceptances, Recruits.list_coordination(user_id, :hiring_decision))
+    |> assign(:acceptance_members, Recruits.list_coordination_members(user_id, :hiring_decision))
     |> assign(:acceptance, nil)
     |> then(&{:ok, &1})
   end
