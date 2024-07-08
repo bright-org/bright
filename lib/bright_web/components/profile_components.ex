10db183977351fc8a04e2be2704b7579c7b5da0f
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-07-09 07:22:26 +0900

    クラスの調整

--- lib/bright_web/components/profile_components.ex
+++ lib/bright_web/components/profile_components.ex
@@ -226,7 +226,7 @@ defmodule BrightWeb.ProfileComponents do
         </div>
         <div class="flex flex-col lg:flex-row gap-x-4 gap-y-2 lg:gap-y-0">
           <span class="text-sm lg:text-normal w-14">クラス<%= if @skill_class, do: @skill_class.class, else: "" %></span>
-          <span class="text-sm lg:text-normal w-[330px] break-all"><%= if @skill_class, do: @skill_class.name, else: ""  %></span>
+          <span class="text-sm lg:text-normal w-[360px] break-all"><%= if @skill_class, do: @skill_class.name, else: ""  %></span>
         </div>
       </div>
     """
