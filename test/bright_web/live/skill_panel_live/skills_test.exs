19e8af8313d000f83473d35d773d47f8dc3e056e
koyo-miyamura <nexus9071@gmail.com>
2024-07-13 15:21:25 +0900

    テスト記述

--- test/bright_web/live/skill_panel_live/skills_test.exs
+++ test/bright_web/live/skill_panel_live/skills_test.exs
@@ -260,7 +260,29 @@ defmodule BrightWeb.SkillPanelLive.SkillsTest do
       assert has_element?(show_live, "#share-button-group-facebook")
     end
 
-    test "does not show sns share button when other user", %{conn: conn, user: user} do
+    test "shows qr share button", %{conn: conn, skill_panel: skill_panel} do
+      {:ok, show_live, _html} = live(conn, ~p"/panels/#{skill_panel}")
+
+      assert has_element?(show_live, "#share_qr_icon_button")
+      refute has_element?(show_live, "#qr-code-modal")
+
+      element(show_live, "#share_qr_icon_button")
+      |> render_click()
+
+      assert has_element?(show_live, "#qr-code-modal")
+
+      {:ok, show_live, _html} = live(conn, ~p"/panels")
+
+      assert has_element?(show_live, "#share_qr_icon_button")
+      refute has_element?(show_live, "#qr-code-modal")
+
+      element(show_live, "#share_qr_icon_button")
+      |> render_click()
+
+      assert has_element?(show_live, "#qr-code-modal")
+    end
+
+    test "does not show sns and qr share button when other user", %{conn: conn, user: user} do
       %{user: user_2, skill_panel: skill_panel_2} = create_user_with_skill()
       user_2 |> with_user_profile()
       create_team_with_team_member_users([user, user_2])
@@ -270,9 +292,10 @@ defmodule BrightWeb.SkillPanelLive.SkillsTest do
       refute has_element?(show_live, "#share-button-group")
       refute has_element?(show_live, "#share-button-group-twitter")
       refute has_element?(show_live, "#share-button-group-facebook")
+      refute has_element?(show_live, "#share_qr_icon_button")
     end
 
-    test "does not show sns share button when anon user", %{conn: conn} do
+    test "does not show sns and qr share button when anon user", %{conn: conn} do
       %{user: user_2, skill_panel: skill_panel_2} = create_user_with_skill()
       encrypted_name = BrightWeb.DisplayUserHelper.encrypt_user_name(user_2)
 
@@ -281,6 +304,7 @@ defmodule BrightWeb.SkillPanelLive.SkillsTest do
       refute has_element?(show_live, "#share-button-group")
       refute has_element?(show_live, "#share-button-group-twitter")
       refute has_element?(show_live, "#share-button-group-facebook")
+      refute has_element?(show_live, "#share_qr_icon_button")
     end
   end
 
