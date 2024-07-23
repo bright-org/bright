1d2a1ff7fa6045f3b0c215db563d4c513966d80d
thehaigo <sirannha@gmail.com>
2024-07-23 17:07:06 +0900

    ガイドメッセージ変更、わかりにくい、わかりやすいを削除

--- lib/bright_web/live/help_message_component.ex
+++ lib/bright_web/live/help_message_component.ex
@@ -19,23 +19,6 @@ defmodule BrightWeb.HelpMessageComponent do
       <div class="relative px-4 py-2 my-1 rounded text-sm bg-designer-dazzle leading-normal">
         <%= render_slot(@inner_block) %>
 
-        <div class="flex gap-4 justify-center pt-2">
-          <button
-            id={good_button_id(@id)}
-            type="button"
-            class="btn-help-good bg-brightGray-900 border border-brightGray-900 font-bold p-1 rounded text-white w-48"
-            phx-click={JS.push("good", target: @myself) |> hide("##{@id}")}>
-            この説明は分かりやすい
-          </button>
-          <button
-            id={bad_button_id(@id)}
-            type="button"
-            class="btn-help-bad bg-white border border-brightGray-900 font-bold p-1 rounded text-brightGray-900 w-24"
-            phx-click={JS.push("bad", target: @myself) |> hide("##{@id}")}>
-            わかりにくい
-          </button>
-        </div>
-
         <button
           class="absolute top-2 right-2"
           phx-click={JS.push("close", target: @myself) |> hide("##{@id}")}>
@@ -62,18 +45,4 @@ defmodule BrightWeb.HelpMessageComponent do
   def handle_event("close", _params, socket) do
     {:noreply, assign(socket, :open, false)}
   end
-
-  def handle_event("good", _params, socket) do
-    {:noreply, assign(socket, :open, false)}
-  end
-
-  def handle_event("bad", _params, socket) do
-    {:noreply, assign(socket, :open, false)}
-  end
-
-  # NOTE:
-  #   idはGAイベントトラッキング対象、変更の際は確認と共有必要
-  defp good_button_id(id), do: "btn-#{id}-good"
-
-  defp bad_button_id(id), do: "btn-#{id}-bad"
 end
