356fc13095b52fd2386ca5bbc61567b04588148d
thehaigo <sirannha@gmail.com>
2024-07-31 16:54:38 +0900

    スキルを選択するを新デザインに変更

--- lib/bright_web/live/onboarding_live/index.ex
+++ lib/bright_web/live/onboarding_live/index.ex
@@ -1,39 +1,29 @@
 defmodule BrightWeb.OnboardingLive.Index do
   use BrightWeb, :live_view
 
+  alias Bright.SkillPanels
   alias Bright.Onboardings
   alias Bright.Onboardings.UserOnboarding
 
-  @default_tab "engineer"
-  @panels %{
-    "want_todo_panel" => :open_want_todo,
-    "wants_job_panel" => :open_wants_job
-  }
-
   @impl true
   def mount(_params, _session, socket) do
+    scores = SkillPanels.list_skill_panels_with_score(socket.assigns.current_user.id)
+
     socket
     |> assign(:open_want_todo, false)
     |> assign(:open_wants_job, false)
-    |> assign(:tab, @default_tab)
+    |> assign(:scores, scores)
     |> then(&{:ok, &1})
   end
 
   @impl true
-  def handle_params(params, uri, socket) do
+  def handle_params(_, uri, socket) do
     current_path = URI.parse(uri).path
 
     socket
     |> assign(:current_path, current_path)
     |> assign(:page_title, page_title(current_path))
-    |> assign_params(params)
     |> then(&{:noreply, &1})
-  end
-
-  defp assign_params(socket, params) do
-    socket
-    |> assign(:open, Map.get(params, "open", false))
-    |> assign(:tab, Map.get(params, "tab", "engineer"))
   end
 
   @impl true
@@ -46,32 +36,9 @@ defmodule BrightWeb.OnboardingLive.Index do
     |> then(&{:noreply, &1})
   end
 
-  @impl true
-  def handle_event("toggle_panel", %{"panel" => panel}, socket) do
-    socket
-    |> assign(@panels[panel], !Map.get(socket.assigns, @panels[panel]))
-    |> assign(@panels[hide_panel(panel)], false)
-    |> then(&{:noreply, &1})
+  def handle_event("request", _params, socket) do
+    {:noreply, put_flash(socket, :info, "ジョブパネルのリクエストを受け付けました")}
   end
-
-  def toggle(js \\ %JS{}, id) do
-    js
-    |> JS.push("toggle_panel", value: %{panel: id})
-    |> JS.toggle(to: "##{id}")
-    |> JS.hide(to: "##{hide_panel(id)}")
-  end
-
-  def hide_panel(id) do
-    Map.keys(@panels)
-    |> Enum.reject(&(&1 == id))
-    |> List.first()
-  end
-
-  def close(),
-    do: "before:-mt-2 before:rotate-225"
-
-  def open(),
-    do: "rounded-bl-none rounded-br-none before:-mt-0.5 before:rotate-45"
 
   defp skip_onboarding(nil, user_id) do
     {:ok, _onboarding} =
@@ -84,7 +51,7 @@ defmodule BrightWeb.OnboardingLive.Index do
   defp skip_onboarding(%UserOnboarding{}, _), do: false
 
   defp page_title(<<"/onboardings", _rest::binary>>), do: "オンボーディング"
-  defp page_title(<<"/more_skills", _rest::binary>>), do: "スキルを選ぶ"
+  defp page_title(<<"/more_skills", _rest::binary>>), do: "ジョブパネル"
 
   def hidden_more_skills(current_path) do
     if String.match?(current_path, ~r/onboarding/), do: "", else: "hidden"
--- lib/bright_web/live/onboarding_live/wants_job_components.ex
+++ lib/bright_web/live/onboarding_live/wants_job_components.ex
@@ -4,73 +4,107 @@ defmodule BrightWeb.OnboardingLive.WantsJobComponents do
   alias Bright.{Jobs, CareerFields}
   alias Bright.Jobs.Job
 
-  @rank %{entry: "入門", basic: "基本", advanced: "応用", expert: "高度"}
+  @default_pos "engineer"
+  @rank %{entry: "基礎", basic: "基本", advanced: "応用", expert: "高度"}
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div id="wants_job_panel" class="p-4 lg:p-8">
-      <!-- タブここから -->
-      <aside id="select_job">
-        <ul class="flex relative text-xs">
+    <div class="flex flex-col lg:flex-row">
+      <div class="bg-white rounded w-full order-2 mt-4 lg:mt-0 lg:order-1">
+        <div id="wants_job_panel" class="p-4">
           <%= for career_field <- @career_fields do %>
-            <li
-              class={
-                  "cursor-pointer select-none py-2 text-center w-40 " <>
-                  add_edge_style(@career_fields, career_field) <>
-                  if @selected_career.name_en == career_field.name_en,
-                    do: "bg-#{career_field.name_en}-dark text-white",
-                    else: "bg-#{career_field.name_en}-dazzle hover:bg-#{career_field.name_en}-dark text-brightGray-200 hover:text-white"
-                }
-              phx-click={JS.push("tab_click", target: @myself, value: %{id: career_field.id})}
-            >
-              <%= career_field.name_ja %>
-            </li>
-          <% end %>
-          <!-- αは落とす -->
-          <li :if={false}>
-            <a
-              href="#"
-              class="absolute bg-brightGray-900 block cursor-pointer font-bold select-none py-2 right-0 rounded text-center text-white -top-1.5 w-48 hover:filter hover:brightness-[80%]"
-            >
-              キャリアパスを見直す
-            </a>
-          </li>
-        </ul>
-      </aside>
-      <!-- タブここまで -->
+            <div id={career_field.name_en}>
+              <h3 class={"border-l-4 px-2 border-#{career_field.name_en}-dark"}><%= career_field.name_ja %></h3>
+              <%= for rank <- Ecto.Enum.values(Job, :rank) do %>
+                <div class="my-8">
+                  <p class="text-left text-[#777777] text-xl"><%= @rank[rank] %></p>
+                  <hr class="h-[2px] bg-brightGray-50 mb-4" />
+                  <div class="flex flex-wrap justify-center mt-2 lg:justify-start">
+                    <% jobs = Map.get(@jobs, career_field.name_en, %{}) %>
+                    <%= for job <- Map.get(jobs, rank, []) do %>
+                      <%= if Enum.count(job.skill_panels) == 0 do %>
+                        <div class="border-[3px] px-4 py-2 m-2 rounded w-[330px] h-40 flex flex-col bg-brightGray-50">
+                          <div class="flex justify-between">
+                            <p class="font-bold mb-2 w-44 truncate text-[#777777] opacity-85"><%= job.name %></p>
+                            <button
+                              class="rounded-lg border bg-white py-1 px-2 -mt-1 mb-2 text-xs"
+                              phx-click="request"
+                              phx-value-job={job.id}
+                            >
+                              リクエスト
+                            </button>
+                          </div>
+                          <hr />
+                          <p class="mt-2 text-[#777777] opacity-85 h-[40px] overflow-hidden">
+                            <%= String.split(job.description, "。") |> List.first() %>
+                          </p>
+                        </div>
+                      <% else %>
+                        <% panel_id = List.first(job.skill_panels) |> Map.get(:id, nil) %>
+                        <% score = Enum.find(@scores, & &1.id == panel_id) %>
+                        <.link navigate={"#{@current_path}/jobs/#{job.id}"}>
+                          <div class={"border-[3px] px-4 py-2 m-2 ounded w-[330px] h-40 flex flex-col #{if is_nil(score), do: "", else: "border-brightGreen-300"}"}>
+                            <div class="flex justify-between mt-2">
+                              <p class="font-bold mb-2 w-48 truncate"><%= job.name %></p>
 
-      <!-- ジョブセクションここから -->
-      <section>
-        <%= if @selected_career do %>
-        <section
-          class={"bg-#{@selected_career.name_en}-dazzle px-4 py-4"}
-        >
-          <%= for rank <- Ecto.Enum.values(Job, :rank) do %>
-          <div class="mb-8">
-            <p class="font-bold text-center lg:text-left"><%= @rank[rank] %></p>
-            <ul class="flex flex-wrap gap-4 justify-center mt-2 lg:justify-start">
-
-              <% jobs = Map.get(@jobs, @selected_career.name_en, %{}) %>
-              <%= for job <- Map.get(jobs, rank, []) do %>
-              <li>
-                <.link navigate={"#{@current_path}/jobs/#{job.id}"} class="block">
-                  <label
-                    class={"bg-#{@selected_career.name_en}-dark block border border-solid border-#{@selected_career.name_en}-dark cursor-pointer font-bold px-2 rounded select-none text-white text-center hover:filter hover:brightness-[80%] min-w-[220px] h-10 leading-10"}
-                  >
-                    <%= job.name %>
-                  </label>
-                </.link>
-              </li>
+                                <%= if is_nil(score) do %>
+                                <p class="flex gap-x-2 h-8 mb-2 -mt-2">
+                                  <img src={icon_path(:none)} width="20" height="23" />
+                                  <img src={icon_path(:none)} width="20" height="23" />
+                                  <img src={icon_path(:none)} width="20" height="23" />
+                                  </p>
+                                <% else %>
+                                <p class="flex gap-x-2 h-8 mb-2">
+                                    <%= for class <- score.skill_classes do %>
+                                      <% class_score = List.first(class.skill_class_scores)%>
+                                      <%= if is_nil(class_score) do %>
+                                        <.link >
+                                          <img src={icon_path(:none)} />
+                                        </.link>
+                                      <% else %>
+                                        <.link navigate={~p"/panels/#{panel_id}?class=#{class.class}"} >
+                                          <img src={icon_path(class_score.level)}  />
+                                        </.link>
+                                      <% end %>
+                                    <% end %>
+                                  </p>
+                                <% end %>
+                            </div>
+                            <hr />
+                            <p class="mt-2 text-[#777777] text-sm opacity-85 h-[60px] overflow-scroll">
+                              <%= String.split(job.description, "。") |> List.first() %>
+                            </p>
+                            <div class="flex gap-x-2 mt-2">
+                              <%= for tag <-  job.career_fields do %>
+                                <p class={"border rounded-full p-1 text-xs text-#{tag.name_en}-dark bg-#{tag.name_en}-light"}>
+                                  <%= tag.name_ja %>
+                                </p>
+                              <% end %>
+                            </div>
+                          </div>
+                        </.link>
+                      <% end %>
+                    <% end %>
+                  </div>
+                </div>
               <% end %>
-              <!-- ジョブここまで -->
-            </ul>
-          </div>
+            </div>
           <% end %>
-        </section>
+        </div>
+      </div>
+      <div class="w-full lg:w-60 lg:ml-12 bg-white h-full p-2 sticky top-16 lg:top-2 order-1 lg:order-2 flex flex-row lg:flex-col">
+        <%= for career_field <- @career_fields do %>
+          <p
+            class={"cursor-default px-4 py-2 lg:mb-2 text-xs lg:text-lg text-[#004D36] #{if @pos == career_field.name_en, do: "border-l-4 border-#{career_field.name_en}-dark bg-#{career_field.name_en}-light", else: "ml-1"}"}
+            phx-click="scroll_to"
+            phx-value-pos={career_field.name_en}
+            phx-target={@myself}
+          >
+            <%= career_field.name_ja %>
+          </p>
         <% end %>
-      </section>
-      <!-- ジョブセクション ここまで -->
+      </div>
     </div>
     """
   end
@@ -82,38 +116,31 @@ defmodule BrightWeb.OnboardingLive.WantsJobComponents do
     socket
     |> assign(:rank, @rank)
     |> assign(:jobs, jobs)
+    |> assign(:pos, @default_pos)
     |> then(&{:ok, &1})
   end
 
   @impl true
-  def update(%{tab: tab} = assigns, socket) do
+  def update(assigns, socket) do
     career_fields = CareerFields.list_career_fields()
 
     socket
     |> assign(assigns)
     |> assign(:career_fields, career_fields)
-    |> assign(:selected_career, Enum.find(career_fields, &(&1.name_en == tab)))
     |> then(&{:ok, &1})
   end
 
   @impl true
-  def handle_event(
-        "tab_click",
-        %{"id" => career_field_id},
-        %{assigns: %{career_fields: career_fields}} = socket
-      ) do
+  def handle_event("scroll_to", %{"pos" => pos}, socket) do
     socket
-    |> assign(:selected_career, Enum.find(career_fields, fn c -> c.id == career_field_id end))
+    |> assign(:pos, pos)
+    |> push_event("scroll-to", %{"id" => pos})
     |> then(&{:noreply, &1})
   end
 
-  defp add_edge_style(career_fields, career_field) do
-    index = Enum.find_index(career_fields, &(&1 == career_field))
-
-    case index do
-      0 -> "rounded-tl "
-      3 -> "rounded-tr "
-      _ -> ""
-    end
-  end
+  defp icon_base_path(file), do: "/images/common/icons/#{file}"
+  defp icon_path(:none), do: icon_base_path("gemGray.svg")
+  defp icon_path(:beginner), do: icon_base_path("jemLow.svg")
+  defp icon_path(:normal), do: icon_base_path("jemMiddle.svg")
+  defp icon_path(:skilled), do: icon_base_path("jemHigh.svg")
 end
