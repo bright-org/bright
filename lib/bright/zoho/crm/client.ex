f33a8b3083b88e90a12e749ef9094aa834887a59
koyo-miyamura <nexus9071@gmail.com>
2024-08-05 03:37:36 +0900

    Add comment

--- lib/bright/zoho/crm/client.ex
+++ lib/bright/zoho/crm/client.ex
@@ -3,6 +3,14 @@ defmodule Bright.Zoho.Crm.Client do
   Zoho CRM の API クライアント
   """
 
+  @doc """
+  クライアントを生成する
+
+  ## Examples
+
+      iex> Bright.Zoho.Crm.Client.new(%{api_domain: "https://www.zohoapis.jp", access_token: "xxxxx"})
+      %Tesla.Client{}
+  """
   def new(%{api_domain: api_domain, access_token: access_token}) do
     middleware = [
       {Tesla.Middleware.BaseUrl, "#{api_domain}/crm/v6"},
@@ -13,6 +21,9 @@ defmodule Bright.Zoho.Crm.Client do
     Tesla.client(middleware)
   end
 
+  @doc """
+  「連絡先」を1件作成する
+  """
   def create_contact(client, data) do
     client |> Tesla.post("/Contacts", data)
   end
