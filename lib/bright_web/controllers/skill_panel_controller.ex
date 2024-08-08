bfb9160e736810517f170d6178e34f68e2e9327a
thehaigo <sirannha@gmail.com>
2024-08-08 14:12:39 +0900

    なかったときの作成を削ってt/fを返すだけに変更

--- lib/bright_web/controllers/skill_panel_controller.ex
+++ lib/bright_web/controllers/skill_panel_controller.ex
@@ -10,15 +10,21 @@ defmodule BrightWeb.SkillPanelController do
 
   def get_skill_panel(conn, %{"skill_panel_id" => skill_panel_id}) do
     panel = SkillPanels.get_skill_panel!(skill_panel_id)
+    user = conn.assigns.current_user
 
     case UserSkillPanels.user_skill_panel_exists?(
-           conn.assigns.current_user.id,
+           user.id,
            panel.id
          ) do
       true ->
         redirect(conn, to: ~p"/panels/#{panel.id}")
 
       false ->
+        UserSkillPanels.create_user_skill_panel(%{
+          user_id: user.id,
+          skill_panel_id: skill_panel_id
+        })
+
         conn
         |> put_flash(:info, "スキルパネル:#{panel.name}を取得しました")
         |> redirect(to: ~p"/panels/#{panel.id}")
