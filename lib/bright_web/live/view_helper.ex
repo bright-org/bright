866e08abe84913587ed7b9a96f2fe5b38ac95ed8
thehaigo <sirannha@gmail.com>
2024-08-08 23:34:36 +0900

    add module doc

--- lib/bright_web/live/view_helper.ex
+++ lib/bright_web/live/view_helper.ex
@@ -1,4 +1,8 @@
 defmodule BrightWeb.ViewHelper do
+  @moduledoc """
+  汎用的な表示用のヘルパー
+  """
+
   def format_datetime(naive_datetime, "Asia/Tokyo") do
     naive_datetime
     |> NaiveDateTime.add(9, :hour)
