3f9d99dbf3d385b097beef902d8e17f47fdaba11
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-08-21 18:01:53 +0900

    SkillSharaOgpComponent位置修正

--- lib/bright_web/components/skill_shara_ogp_component.ex
+++ lib/bright_web/components/skill_shara_ogp_component.ex
@@ -9,8 +9,10 @@ defmodule BrightWeb.SkillSharaOgpComponent do
     ~H"""
      <div class="absolute flow -left-[1200px] w-[1200px]">
       <p class="hidden" id="skill_shara_og_image_data" phx-click="skill_shara_og_image_data_click" />
-      <div id="skill_shara_og_image" class="pt-5 px-80 min-h-[630px]">
-      <.triangle_graph data={@data} id="ogp_triangle_graph"/>
+      <div id="skill_shara_og_image" class="min-h-[630px] min-w-[1200px] content-center">
+        <div class="flex justify-center">
+          <.triangle_graph data={@data} id="ogp_triangle_graph"/>
+        </div>
       </div>
     </div>
     """
